5d461c48edbe2c6dd9bccb052ed7a5078d3e79e5
Drake Bott <bott.da@gmail.com>
2022-03-20 21:27:44 -0500

    Transparent theme

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,8 +1,24 @@
 -- Nightfox has multiple variants, one must be loaded for theme to work
 -- nordfox, dayfox, dawnfox, duskfox
-require('nightfox').load('duskfox')
+vim.cmd("colorscheme nightfox");
+
+-- require('nightfox').load('duskfox')
 require("indent_blankline").setup {
     space_char_blankline = " ",
     show_current_context = true,
     show_current_context_start = true,
 }
+
+require("transparent").setup({
+enable = true, -- boolean: enable transparent
+extra_groups = { -- table/string: additional groups that should be clear
+    -- example of akinsho/nvim-bufferline.lua
+    -- "BufferLineTabClose",
+    -- "BufferlineBufferSelected",
+    -- "BufferLineFill",
+    -- "BufferLineBackground",
+    -- "BufferLineSeparator",
+    -- "BufferLineIndicatorSelected",
+  },
+  exclude = {}, -- table: groups you don't want to clear
+})
