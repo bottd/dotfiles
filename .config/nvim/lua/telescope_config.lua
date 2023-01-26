dfee2a984d9cb8494f33ae45fb952f738cc8188a
Drake Bott <bottd@users.noreply.github.com>
2023-01-26 15:19:57 -0600

    Update nvim lsp deps

--- .config/nvim/lua/telescope_config.lua
+++ .config/nvim/lua/telescope_config.lua
@@ -11,8 +11,9 @@ require('telescope').setup {
   }
 }
 -- To get fzf loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
+--load_extension, somewhere after setup function:
 require('telescope').load_extension('fzf')
 
 nnoremap('<leader>ff', ':Telescope find_files<Cr>');
 nnoremap('<leader>fs', ':Telescope live_grep<Cr>');
+nnoremap('<leader>fk', ':Telescope keymaps<Cr>');
