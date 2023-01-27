8c42a395b7c83a38909dfee80ab95b2b390dbf9c
Drake Bott <bottd@users.noreply.github.com>
2023-01-27 12:47:54 -0600

    Update nvim config, repair tmux vim navigation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,8 +3,7 @@
 -- Configure tree sitter
 -- Install nvim-cmp
 require('plugins');
-require'mapx'.setup{ global = true }
-require('default_settings');
+require('general_config');
 require('statusline');
 require('filetree');
 require('theme');
@@ -13,3 +12,4 @@ require('git');
 require('lsp_config');
 require('telescope_config');
 require('treesitter_config');
+require('which_key');
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -6,11 +6,11 @@ local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   PACKER_BOOTSTRAP = fn.system({
-	  'git', 
-	  'clone', 
-	  '--depth', 
-	  '1', 
-	  'https://github.com/wbthomason/packer.nvim', 
+	  'git',
+	  'clone',
+	  '--depth',
+	  '1',
+	  'https://github.com/wbthomason/packer.nvim',
 	  install_path
   })
   print('Installing packer close and reopen Neovim...')
@@ -24,7 +24,6 @@ vim.cmd([[
     autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
 ]])
-
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, 'packer')
 if not status_ok then
@@ -46,12 +45,8 @@ return packer.startup(function(use)
   -- Plugin manager, packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  -- An implementation of the Popup API from vim in Neovim
-  use 'nvim-lua/popup.nvim'
-
   -- Useful lua functions used ny lots of plugins
   use 'nvim-lua/plenary.nvim'
-  use 'b0o/mapx.nvim'
 
   -- File tree
   use {
@@ -70,7 +65,6 @@ return packer.startup(function(use)
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     'neovim/nvim-lspconfig',
-    'j-hui/fidget.nvim'
   }
 
   -- Completion
@@ -83,12 +77,27 @@ return packer.startup(function(use)
   use 'saadparwaiz1/cmp_luasnip'
 
   -- Telescope
-  use 'nvim-telescope/telescope.nvim'
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {
+      'debugloop/telescope-undo.nvim'
+    }
+  }
+
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
   -- Status line
-  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true }}
-  use { 
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {
+      'kyazdani42/nvim-web-devicons',
+      'arkav/lualine-lsp-progress',
+      opt = true
+    }
+  }
+
+  -- Which key
+  use {
     'folke/which-key.nvim',
     config = function()
       vim.o.timeout = true
@@ -102,10 +111,9 @@ return packer.startup(function(use)
   use 'EdenEast/nightfox.nvim'
   use 'lukas-reineke/indent-blankline.nvim'
   use 'xiyaowong/nvim-transparent'
-  use 'vv9k/bogster'
 
   -- Treesitter
-  use { 'nvim-treesitter/nvim-treesitter', 
+  use { 'nvim-treesitter/nvim-treesitter',
         run = function()
             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
             ts_update()
@@ -113,7 +121,7 @@ return packer.startup(function(use)
       }
 
   -- Git plugins
-  -- use 'lewis6991/gitsigns.nvim'
+  use 'lewis6991/gitsigns.nvim'
 
   -- Copilot
   -- :Copilot setup to authenticate with github
