88e9719bc75f213d22fa63fca52b5cefe7a11f93
Drake Bott <bottd@users.noreply.github.com>
2023-02-10 16:29:14 -0600

    Add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+alias vim nvim
+
+set -gx LAV_PRECOMMIT true
+set -gx AWS_REGION 'us-east-1'
+set -gx PATH "$HOME/.jenv/bin:$PATH"
+# eval "$(jenv init -)"
+set PATH $HOME/.cargo/bin /opt/homebrew/bin $PATH
+
+
+set -gx NVM_DIR "$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+set -gx JIRA_API_TOKEN "redacted"
