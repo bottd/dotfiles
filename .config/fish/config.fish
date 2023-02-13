0ef7d6d6d3ffca4843ebd9f4618ed12ee683fb3e
Drake Bott <bottd@users.noreply.github.com>
2023-02-13 15:45:06 -0600

    Remove expired jira token, switch to duskfox

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,10 @@ if status is-interactive
 end
 
 starship init fish | source
+
 alias vim nvim
+
+source $HOME/.config/fish/config.(hostname).fish
 
 set -gx LAV_PRECOMMIT true
 set -gx AWS_REGION 'us-east-1'
@@ -15,4 +18,3 @@ set PATH $HOME/.cargo/bin /opt/homebrew/bin $PATH
 set -gx NVM_DIR "$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-set -gx JIRA_API_TOKEN "ATATT3xFfGF0pRvLs_VdMLq_HnTuvOx2KNWzd9tiCsbtb3oBcsFoi4258xEL4h85A_g0J_kK01ObiokiARkz7WjNEgJD3qVyl3mcwST6KBgI1fAwPoO3rmx3qW7oZyRN9LBFt8kgze50DG92KbBLKI-YVIEQSid_hYOGuO0QTpxahQXJPRY0kcw=A16C1000"
