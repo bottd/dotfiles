11f512ae18e5617e0d30b3839c80091840ea1b98
Drake Bott <bottd@users.noreply.github.com>
2023-03-26 13:17:12 -0500

    Use bass for nvm loading, nvm package failed to work with alias command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,7 +14,6 @@ set -gx PATH "$HOME/.jenv/bin:$PATH"
 # eval "$(jenv init -)"
 set PATH $HOME/.cargo/bin /opt/homebrew/bin $PATH
 
-
-set -gx NVM_DIR "$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+function nvm
+   bass source $HOME/.nvm/nvm.sh --no-use ';' nvm $argv
+end
