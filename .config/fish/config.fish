8491c903ef97d0c713b0228c841405d2b7e01fc2
Drake Bott <bottd@users.noreply.github.com>
2023-03-29 09:29:24 -0500

    Remove work machine variables, relocated to local config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,12 +8,13 @@ alias vim nvim
 
 source $HOME/.config/fish/config.(whoami).fish
 
-set -gx LAV_PRECOMMIT true
-set -gx AWS_REGION 'us-east-1'
-set -gx PATH "$HOME/.jenv/bin:$PATH"
-# eval "$(jenv init -)"
+# Add cargo to path
 set PATH $HOME/.cargo/bin /opt/homebrew/bin $PATH
+
+# eval $(github-copilot-cli alias -- /opt/homebrew/bin/fish)
+# bass eval "$(github-copilot-cli alias -- "$0")"
 
 function nvm
    bass source $HOME/.nvm/nvm.sh --no-use ';' nvm $argv
 end
+
