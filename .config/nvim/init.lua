c79c470edd8d04128599cd32a4d7867271b4fc06
Drake Bott <bottd@users.noreply.github.com>
2023-07-14 14:22:29 -0500

    Enable dotfiles in nvim tree, hack for yank to clipboard bind

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,7 @@ require('general_config');
 require('plugin_manager');
 -- lazy loads plugins from /lua/plugins/*.lua files
 require('lazy').setup('plugins')
+
+-- Copy to clipboard
+vim.keymap.set('n', '<Leader>y', '"+y');
+
