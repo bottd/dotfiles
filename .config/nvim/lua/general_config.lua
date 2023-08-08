6d5ca7adfbbd3cb3899840ab6257d28e7c049cc1
Drake Bott <dbott@irisidentityprotection.com>
2023-08-08 16:17:46 -0500

    Adds neorg

--- .config/nvim/lua/general_config.lua
+++ .config/nvim/lua/general_config.lua
@@ -8,6 +8,7 @@ vim.api.nvim_command('set cursorline')
 vim.api.nvim_command('set smartindent')
 vim.opt.expandtab = true
 vim.opt.tabstop = 2 vim.opt.shiftwidth = 2
+vim.opt.foldlevelstart = 99
 
 vim.g.tmux_navigator_no_mappints = 1;
 vim.keymap.set('n', '<C-h>', ':<C-U>TmuxNavigateLeft<Cr>')
