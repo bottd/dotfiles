7a4f2048599e4644297078e9edc73b826d1fe56e
Drake Bott <bottd@users.noreply.github.com>
2023-09-09 14:55:26 -0500

    Migrate lsp/cmp config to lsp-zero

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,72 @@
+return {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'L3MON4D3/LuaSnip',
+      {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {}
+      },
+      {
+        'zbirenbaum/copilot.lua',
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+          require("copilot").setup({
+            -- disabling due to using copilot-cmp
+            suggestion = { enabled = false },
+            panel = { enabled = false }
+          })
+        end
+      },
+      {
+        'zbirenbaum/copilot-cmp',
+        -- after = { 'copilot.lua' },
+        config = function()
+          require('copilot_cmp').setup {
+            method = 'getCompletionsCycling'
+          }
+        end
+      }
+    },
+    config = function()
+      -- Here is where you configure the autocompletion settings.
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_cmp()
+
+      -- And you can configure cmp even more, if you want to.
+      local cmp = require('cmp')
+      local cmp_action = lsp_zero.cmp_action()
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+
+      cmp.setup({
+        formatting = lsp_zero.cmp_format(),
+        mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+          -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'copilot' },
+          { name = 'nvim_lsp' },
+          { name = "path" },
+          { name = 'luasnip' },
+          { name = 'buffer' }
+        })
+      })
+    end
+}
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,42 +1,73 @@
 return {
-  'folke/neodev.nvim',
-  'mfussenegger/nvim-dap',
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'dev-v3',
+    lazy = true,
+    config = false,
+    init = function()
+      -- Disable automatic setup, we are doing it manually
+      vim.g.lsp_zero_extend_cmp = 0
+      vim.g.lsp_zero_extend_lspconfig = 0
+    end,
+  },
+  {
+    'williamboman/mason.nvim',
+    lazy = false,
+    config = true
+  },
   {
     'neovim/nvim-lspconfig',
-    config = function()
-      require('config/lsp')
-    end,
+    cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+    event = {'BufReadPre', 'BufNewFile'},
     dependencies = {
-      {
-        'williamboman/mason.nvim',
-        config = function()
-          require("mason").setup {
-            ui = {
-              icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
-              }
-            }
-          }
-        end
-      },
-      {
-        'WhoIsSethDaniel/mason-tool-installer.nvim',
-        config = function()
-          require('mason-tool-installer').setup {
-            ensure_installed = {
-              "node-debug2-adapter",
-              "eslint_d",
-              "markdownlint",
-              "prettier"
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'williamboman/mason-lspconfig.nvim'},
+    },
+    config = function()
+      -- This is where all the LSP shenanigans will live
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_lspconfig()
+
+      lsp_zero.on_attach(function(client, bufnr)
+        -- see :help lsp-zero-keybindings
+        -- to learn the available actions
+        lsp_zero.default_keymaps({buffer = bufnr})
+      end)
+
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+            'cssls',
+            'eslint',
+            'graphql',
+            'html',
+            -- TODO: if using jdtls get plugin
+            -- https://github.com/mfussenegger/nvim-jdtls
+            -- 'jdtls',
+            'jsonnet_ls',
+            'pyright',
+            'rust_analyzer',
+            'sqlls',
+            'stylelint_lsp',
+            sumneko_lua = {
+              Lua = {
+                diagnostics = { globals = { 'vim' } },
+                workspace = { checkThirdParty = false },
+                telemetry = { enable = false },
+              },
             },
-            auto_update = true,
-            run_on_start = true
-          }
-        end
-      },
-      'williamboman/mason-lspconfig.nvim'
-    }
-  },
+            'svelte',
+            'tailwindcss',
+            'tsserver',
+        },
+        handlers = {
+          lsp_zero.default_setup,
+          lua_ls = function()
+            -- (Optional) Configure lua language server for neovim
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+          end,
+        }
+      })
+    end
+  }
 }
