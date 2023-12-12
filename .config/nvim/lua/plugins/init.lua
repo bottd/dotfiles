0b84af8c24a851447b83ac3e5adf35ae3ddc439f
Drake Bott <bott.da@gmail.com>
2023-12-12 15:30:16 -0600

    Update nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,6 +2,7 @@ return {
   -- Useful lua functions used by lots of plugins
   'nvim-lua/plenary.nvim',
   'MunifTanjim/nui.nvim',
+  'stevearc/dressing.nvim',
 
   -- Vim Util plugins
   'gioele/vim-autoswap',
