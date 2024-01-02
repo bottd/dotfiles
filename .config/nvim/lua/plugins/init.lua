6dc2702bf3cd3452add352fdce95a153a2de1593
Drake Bott <bottd@users.noreply.github.com>
2024-01-02 08:15:15 -0700

    Update nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,12 +1,12 @@
 return {
   -- Useful lua functions used by lots of plugins
-  'nvim-lua/plenary.nvim',
-  'MunifTanjim/nui.nvim',
+  "nvim-lua/plenary.nvim",
+  "MunifTanjim/nui.nvim",
 
   -- Vim Util plugins
-  'gioele/vim-autoswap',
+  "gioele/vim-autoswap",
   {
-    'NvChad/nvim-colorizer.lua',
+    "NvChad/nvim-colorizer.lua",
     config = true
   }
 }
