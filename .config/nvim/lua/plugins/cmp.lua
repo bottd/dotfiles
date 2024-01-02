a66121fe23aca86b7b73bfb0613dd895df6d2969
Drake Bott <bottd@users.noreply.github.com>
2024-01-02 12:02:06 -0600

    Add friendly snippets

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -7,6 +7,7 @@ return {
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-cmdline',
       'L3MON4D3/LuaSnip',
+      'rafamadriz/friendly-snippets',
       'f3fora/cmp-spell',
       "Olical/conjure",
       {
