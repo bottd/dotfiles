0dc251fce26504e7e735fb163dc647fac2a00caa
Drake Bott <bottd@users.noreply.github.com>
2024-01-10 15:48:06 -0600

    Update nvim

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,21 +1,21 @@
 return {
   {
-    'nvim-telescope/telescope.nvim',
+    "nvim-telescope/telescope.nvim",
     dependencies = {
-      'debugloop/telescope-undo.nvim',
+      "debugloop/telescope-undo.nvim",
       {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'arch -arm64 make'
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "arch -arm64 make"
       }
     },
     config = function()
-      require('telescope').setup {
+      require("telescope").setup {
         pickers = {
           find_files = {
-            layout_strategy = 'vertical'
+            layout_strategy = "vertical"
           },
           live_grep = {
-            layout_strategy = 'vertical'
+            layout_strategy = "vertical"
           },
         },
         extensions = {
@@ -32,14 +32,16 @@ return {
       }
 
       -- load telescope extensions
-      require('telescope').load_extension('fzf')
-      require('telescope').load_extension('undo')
-      vim.keymap.set('n', '<leader>fb', ':Telescope buffers<Cr>');
-      vim.keymap.set('n', '<leader>ff', ':Telescope find_files<Cr>');
-      vim.keymap.set('n', '<leader>fs', ':Telescope live_grep<Cr>');
-      vim.keymap.set('n', '<leader>fk', ':Telescope keymaps<Cr>');
-      vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<Cr>');
-      vim.keymap.set('n', '<leader>u', ':Telescope undo<Cr>');
-    end
-  }
+      require("telescope").load_extension("fzf")
+      require("telescope").load_extension("undo")
+    end,
+    keys = {
+      { mode = "n", "<leader>fb", ":Telescope buffers<Cr>", desc = "Find Buffer"},
+      { mode = "n", "<leader>ff", ":Telescope find_files<Cr>", desc = "Find File"},
+      { mode = "n", "<leader>fs", ":Telescope live_grep<Cr>", desc = "Find String"},
+      { mode = "n", "<leader>fk", ":Telescope keymaps<Cr>", desc = "Find Keymap"},
+      { mode = "n", "<leader>fh", ":Telescope help_tags<Cr>", desc = "Find Help"},
+      { mode = "n", "<leader>u", ":Telescope undo<Cr>", desc = "Undo Tree"}
+    }
+  },
 }
