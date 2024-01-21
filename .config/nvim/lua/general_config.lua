60f04b9a9e95ee94e4d48533af0b212eaac81370
Drake Bott <bott.da@gmail.com>
2024-01-21 16:38:18 -0600

    Fnl

--- .config/nvim/lua/general_config.lua
+++ .config/nvim/lua/general_config.lua
@@ -1,16 +1,12 @@
--- Vim settings
-vim.api.nvim_command('set number')
-vim.api.nvim_command('set cursorline')
-vim.api.nvim_command('set smartindent')
+-- [nfnl] Compiled from fnl/general_config.fnl by https://github.com/Olical/nfnl, do not edit.
+vim.api.nvim_command("set number")
+vim.api.nvim_command("set cursorline")
+vim.api.nvim_command("set smartindent")
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.foldlevelstart = 99
 vim.opt.spell = true
-vim.opt.spelllang = { 'en_us' }
-
--- Copy to clipboard
-vim.keymap.set('n', '<Leader>y', '"+y', { desc = "Yank to clipboard" });
-
--- Escape terminal mode
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+vim.opt.spelllang = {"en_us"}
+vim.keymap.set("n", "<Leader>y", "\"+y", {desc = "Yank to clipboard"})
+return vim.keymap.set("t", "<Esc>", "<C-\\\\><C-n>")
