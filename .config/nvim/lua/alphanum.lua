8393b24276c8a82b2c649c2437ad9d0fa9ef2749
Drake Bott <bott.da@gmail.com>
2024-02-20 19:32:20 -0600

    Update

--- .config/nvim/lua/alphanum.lua
+++ .config/nvim/lua/alphanum.lua
@@ -1,5 +1,9 @@
 -- [nfnl] Compiled from fnl/alphanum.fnl by https://github.com/Olical/nfnl, do not edit.
-local function file_to_key(filename)
+local function path_to_filename(path)
+  return path:match("^.+/(.+)$")
+end
+local function path_to_key(path)
+  local filename = path_to_filename(path)
   local key = filename:match("^(.-)%-")
   if (key == nil) then
     key = filename:match("^(.-)%.")
@@ -7,12 +11,8 @@ local function file_to_key(filename)
   end
   return key
 end
-local function key_to_list(filename)
-  if (filename == "index.norg") then
-    return {0}
-  else
-  end
-  local key = file_to_key(filename)
+local function key_to_list(path)
+  local key = path_to_filename(path)
   local result = {string.sub(key, 0, 0)}
   local curr_type = "num"
   for i = 1, #key do
@@ -59,39 +59,76 @@ local function key_to_list(filename)
   return result
 end
 local function an_compare(a, b)
+  do
+    local _7_ = {a, b}
+    if ((_G.type(_7_) == "table") and (nil ~= (_7_)[1]) and ((_7_)[2] == nil)) then
+      local x = (_7_)[1]
+      return false
+    elseif ((_G.type(_7_) == "table") and ((_7_)[1] == nil) and (nil ~= (_7_)[2])) then
+      local y = (_7_)[2]
+      return true
+    else
+      local function _8_()
+        local x = (_7_)[1]
+        local y = (_7_)[2]
+        return string.find(x, "index.norg")
+      end
+      if (((_G.type(_7_) == "table") and (nil ~= (_7_)[1]) and (nil ~= (_7_)[2])) and _8_()) then
+        local x = (_7_)[1]
+        local y = (_7_)[2]
+        return false
+      else
+        local function _9_()
+          local x = (_7_)[1]
+          local y = (_7_)[2]
+          return string.find(y, "index.norg")
+        end
+        if (((_G.type(_7_) == "table") and (nil ~= (_7_)[1]) and (nil ~= (_7_)[2])) and _9_()) then
+          local x = (_7_)[1]
+          local y = (_7_)[2]
+          return true
+        else
+        end
+      end
+    end
+  end
+  local a_id = path_to_filename(a)
+  local b_id = path_to_filename(b)
+  local a_keys = key_to_list(a)
+  local b_keys = key_to_list(b)
   local result = nil
-  for k, a_val in pairs(a.keys) do
+  for k, a_val in pairs(a_keys) do
     if not (result == nil) then break end
-    local b_val = b.keys[k]
-    local _8_ = {a_val, b_val}
-    if ((_G.type(_8_) == "table") and (nil ~= (_8_)[1]) and ((_8_)[1] == (_8_)[2])) then
-      local x = (_8_)[1]
+    local b_val = b_keys[k]
+    local _11_ = {a_val, b_val}
+    if ((_G.type(_11_) == "table") and (nil ~= (_11_)[1]) and ((_11_)[1] == (_11_)[2])) then
+      local x = (_11_)[1]
       result = nil
-    elseif ((_G.type(_8_) == "table") and (nil ~= (_8_)[1]) and ((_8_)[2] == nil)) then
-      local x = (_8_)[1]
+    elseif ((_G.type(_11_) == "table") and (nil ~= (_11_)[1]) and ((_11_)[2] == nil)) then
+      local x = (_11_)[1]
       result = false
-    elseif ((_G.type(_8_) == "table") and ((_8_)[1] == nil) and (nil ~= (_8_)[2])) then
-      local y = (_8_)[2]
+    elseif ((_G.type(_11_) == "table") and ((_11_)[1] == nil) and (nil ~= (_11_)[2])) then
+      local y = (_11_)[2]
       result = true
     else
-      local function _9_()
-        local x = (_8_)[1]
-        local y = (_8_)[2]
+      local function _12_()
+        local x = (_11_)[1]
+        local y = (_11_)[2]
         return (x > y)
       end
-      if (((_G.type(_8_) == "table") and (nil ~= (_8_)[1]) and (nil ~= (_8_)[2])) and _9_()) then
-        local x = (_8_)[1]
-        local y = (_8_)[2]
+      if (((_G.type(_11_) == "table") and (nil ~= (_11_)[1]) and (nil ~= (_11_)[2])) and _12_()) then
+        local x = (_11_)[1]
+        local y = (_11_)[2]
         result = false
       else
-        local function _10_()
-          local x = (_8_)[1]
-          local y = (_8_)[2]
+        local function _13_()
+          local x = (_11_)[1]
+          local y = (_11_)[2]
           return (x < y)
         end
-        if (((_G.type(_8_) == "table") and (nil ~= (_8_)[1]) and (nil ~= (_8_)[2])) and _10_()) then
-          local x = (_8_)[1]
-          local y = (_8_)[2]
+        if (((_G.type(_11_) == "table") and (nil ~= (_11_)[1]) and (nil ~= (_11_)[2])) and _13_()) then
+          local x = (_11_)[1]
+          local y = (_11_)[2]
           result = true
         else
         end
@@ -104,22 +141,4 @@ local function an_compare(a, b)
   end
   return result
 end
-local function an_srt(items)
-  local result
-  do
-    local tbl_17_auto = {}
-    local i_18_auto = #tbl_17_auto
-    for k, v in pairs(items) do
-      local val_19_auto = {keys = key_to_list(v), id = v}
-      if (nil ~= val_19_auto) then
-        i_18_auto = (i_18_auto + 1)
-        do end (tbl_17_auto)[i_18_auto] = val_19_auto
-      else
-      end
-    end
-    result = tbl_17_auto
-  end
-  table.sort(result, an_compare)
-  return result
-end
-return {an_srt = an_srt, an_compare = an_compare}
+return {an_compare = an_compare}
