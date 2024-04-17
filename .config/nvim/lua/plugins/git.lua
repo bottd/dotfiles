c61dfb437f2def570728cf4c7fc0ce523a77d8a0
Drake Bott <bottd@users.noreply.github.com>
2024-04-17 09:21:43 -0500

    Add lazygit nvim

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,4 +2,4 @@
 local function _1_()
   return (require("gitsigns")).setup()
 end
-return {"lewis6991/gitsigns.nvim", config = _1_}
+return {{"lewis6991/gitsigns.nvim", config = _1_}, {"kdheepak/lazygit.nvim", cmd = {"LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile"}, dependencies = {"nvim-lua/plenary.nvim"}, keys = {{"<leader>gg", ":LazyGit<cr>", mode = "n", desc = "LazyGit"}}}}
