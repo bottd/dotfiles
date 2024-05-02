ceb472988050b1efaa20b19c1372761d599a6fb1
Drake Bott <bottd@users.noreply.github.com>
2024-05-02 12:14:08 -0500

    Update Rocks configs

--- .config/nvim/lua/plugins_old/diagnostic.lua
+++
@@ -1,2 +0,0 @@
--- [nfnl] Compiled from fnl/plugins/diagnostic.fnl by https://github.com/Olical/nfnl, do not edit.
-return {"folke/trouble.nvim", dependencies = {"nvim-tree/nvim-web-devicons"}, keys = {{"<leader>ci", ":Trouble<Cr>", mode = "n", desc = "Trouble"}}}
--- .config/nvim/lua/plugins_old/init.lua
+++
@@ -1,2 +0,0 @@
--- [nfnl] Compiled from fnl/plugins/init.fnl by https://github.com/Olical/nfnl, do not edit.
-return {"nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim", "gioele/vim-autoswap", {"NvChad/nvim-colorizer.lua", config = true}}
