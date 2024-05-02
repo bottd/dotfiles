52af85dc50f2d9576e191a4edc4e0b7889f964e8
Drake Bott <bott.da@gmail.com>
2024-05-02 12:14:08 -0500

    Update Rocks configs

--- .config/nvim/lua/plugins_old/git.lua
+++
@@ -1,5 +0,0 @@
--- [nfnl] Compiled from fnl/plugins/git.fnl by https://github.com/Olical/nfnl, do not edit.
-local function _1_()
-  return (require("gitsigns")).setup()
-end
-return {{"lewis6991/gitsigns.nvim", config = _1_}, {"kdheepak/lazygit.nvim", cmd = {"LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile"}, dependencies = {"nvim-lua/plenary.nvim"}, keys = {{"<leader>gg", ":LazyGit<cr>", mode = "n", desc = "LazyGit"}}}}
--- .config/nvim/lua/plugins_old/treesitter.lua
+++
@@ -1,6 +0,0 @@
--- [nfnl] Compiled from fnl/plugins/treesitter.fnl by https://github.com/Olical/nfnl, do not edit.
-local function _1_()
-  local ts = require("nvim-treesitter.configs")
-  return ts.setup({ensure_installed = {"comment", "css", "diff", "fennel", "fish", "gitignore", "graphql", "html", "json", "markdown", "markdown_inline", "norg", "lua", "python", "rust", "sql", "svelte", "toml", "tsx", "typescript", "vimdoc"}, autotag = {enable = true}, highlight = {enable = true}})
-end
-return {{"nvim-treesitter/nvim-treesitter", dependencies = {"windwp/nvim-ts-autotag", "nvim-treesitter/nvim-treesitter-context"}, build = ":TSUpdate", config = _1_}}
