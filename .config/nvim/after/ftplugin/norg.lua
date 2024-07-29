296aa551e70d3fae8414058d206e184d83658a67
Drake Bott <bottd@users.noreply.github.com>
2024-07-29 15:24:38 -0500

    Update -- install my first plugin!

--- .config/nvim/after/ftplugin/norg.lua
+++ .config/nvim/after/ftplugin/norg.lua
@@ -3,5 +3,4 @@ vim.opt.conceallevel = 3
 vim.api.nvim_command("set nonumber")
 vim.api.nvim_command("set linebreak")
 vim.api.nvim_command("set breakindent")
-vim.keymap.set("n", "<leader>ff", ":Telescope neorg find_norg_files<Cr>", {desc = "Find Norg Files"})
-return vim.keymap.set("n", "<leader>nw", ":Telescope neorg switch_workspace<Cr>", {desc = "Switch Workspace"})
+return vim.keymap.set("n", "<Cr>", "<Plug>(neorg.esupports.hop.hop-link)")
