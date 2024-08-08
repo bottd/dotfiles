759f430c8daceffeb53b750d8fd8b208a44a45a8
Drake Bott <bott.da@gmail.com>
2024-08-07 21:09:59 -0500

    Conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,3 +1,3 @@
 -- [nfnl] Compiled from fnl/plugins/conform.fnl by https://github.com/Olical/nfnl, do not edit.
 local conform = require("conform")
-return conform.setup({format_on_save = {timeout_ms = 500, lsp_fallback = true}, formatters_by_ft = {fennel = {"fnlfmt"}, javascript = {"prettierd", "prettier"}, lua = {"stylua"}, rust = {"rustfmt"}}})
+return conform.setup({format_on_save = {timeout_ms = 500, lsp_fallback = true}, formatters_by_ft = {css = {"prettierd", "prettier"}, fennel = {"fnlfmt"}, html = {"prettierd", "prettier"}, javascript = {"prettierd", "prettier"}, json = {"prettierd", "prettier"}, lua = {"stylua"}, rust = {"rustfmt"}, svelte = {"prettierd", "prettier"}, toml = {"taplo"}, typescript = {"prettierd", "prettier"}, typescriptreact = {"prettierd", "prettier"}}})
