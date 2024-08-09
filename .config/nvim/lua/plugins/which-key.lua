6719e1b62abb248774d332312991c40e381316b5
Drake Bott <bottd@users.noreply.github.com>
2024-08-09 12:30:24 -0500

    Update

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -2,4 +2,11 @@
 vim.o.timeout = true
 vim.o.timeoutlen = 300
 local which_key = require("which-key")
-return which_key.setup()
+local splits = require("smart-splits")
+local function _1_()
+  return which_key.show({keys = "<C-w>", loop = true})
+end
+local function _2_()
+  return require("which-key.extras")(__fnl_global___2eexpand_2ewin)
+end
+return which_key.add({{"<C-w><space>", _1_}, {"<leader>w", group = "windows", proxy = "<c-w>", expand = _2_}})
