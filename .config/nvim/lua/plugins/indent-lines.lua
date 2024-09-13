95d778c6e992b02cfe5dd6293cf9e627dc5c7a01
Drake Bott <bott.da@gmail.com>
2024-09-13 15:45:34 -0500

    update

--- .config/nvim/lua/plugins/indent-lines.lua
+++ .config/nvim/lua/plugins/indent-lines.lua
@@ -1,5 +1,5 @@
 -- [nfnl] Compiled from fnl/plugins/indent-lines.fnl by https://github.com/Olical/nfnl, do not edit.
 local indentscope = require("mini.indentscope")
-indentscope.setup({symbol = "\226\148\130", options = {try_as_border = true}})
+indentscope.setup({symbol = "\226\148\130", options = {try_as_border = true}, animation = indentscope.gen_animation.linear({easing = "out", duration = 20, unit = "total"})})
 local ibl = require("ibl")
 return ibl.setup({indent = {char = "\226\148\130"}, scope = {enabled = false}})
