b570c01df544b964bce2a884aa889528e61476e4
Drake Bott <me@drake.dev>
2024-09-17 21:08:05 -0500

    update

--- .config/nvim/lua/plugins/care.lua
+++ .config/nvim/lua/plugins/care.lua
@@ -8,9 +8,10 @@ local function _2_(entry, data)
   local get_kind_name = _local_3_["get_kind_name"]
   local _local_4_ = require("care.presets.utils")
   local LabelEntries = _local_4_["LabelEntries"]
-  local completion_item = entry["completion_item"]
-  local _local_5_ = require("care.config").options.ui
-  local type_icons = _local_5_["type_icons"]
+  local _local_5_ = entry
+  local completion_item = _local_5_["completion_item"]
+  local _local_6_ = ((require("care.config")).options).ui
+  local type_icons = _local_6_["type_icons"]
   local entry_kind
   if (type(completion_item.kind) == "string") then
     entry_kind = completion_item.kind
@@ -19,12 +20,12 @@ local function _2_(entry, data)
   end
   return {{{completion_item.label}}, {{(" " .. (type_icons[entry_kind] or type_icons.Text) .. " " .. data.source_name .. ""), ("@care.type.%s"):format(entry_kind)}}}
 end
-local function _7_(body)
-  local _local_8_ = require("luasnip")
-  local lsp_expand = _local_8_["lsp_expand"]
+local function _8_(body)
+  local _local_9_ = require("luasnip")
+  local lsp_expand = _local_9_["lsp_expand"]
   return lsp_expand(body)
 end
-setup({ui = {ghost_text = {position = "inline"}, menu = {max_height = 10, format_entry = _2_}}, alignment = {"left", "right"}, selection_behavior = "insert", confirm_behavior = "replace", sorting_direction = "away-from-cursor", sources = {lsp = {max_entries = 5, priority = 1}, cmp_buffer = {max_entries = 3}}, snippet_expansion = _7_})
+setup({ui = {ghost_text = {position = "inline"}, menu = {max_height = 10, format_entry = _2_}}, alignment = {"left", "right"}, selection_behavior = "insert", confirm_behavior = "replace", sorting_direction = "away-from-cursor", sources = {lsp = {max_entries = 5, priority = 1}, cmp_buffer = {max_entries = 3}}, snippet_expansion = _8_})
 vim.keymap.set("i", "<Cr>", "<Plug>(CareConfirm)")
 vim.keymap.set("i", "<c-e>", "<Plug>(CareClose)")
 vim.keymap.set("i", "<c-n>", "<Plug>(CareSelectNext)")
