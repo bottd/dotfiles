09a31565b19a7740ccffe054652ac60c9af4157d
Drake Bott <bottd@users.noreply.github.com>
2024-09-17 21:08:05 -0500

    update

--- .config/nvim/fnl/plugins/treesitter.fnl
+++ .config/nvim/fnl/plugins/treesitter.fnl
@@ -1,3 +1,6 @@
-(local {: setup} (require :nvim-treesitter.configs))
-(setup {:highlight {:enable true}})
+(local treesitter-context (require :treesitter-context))
+(treesitter-context.setup)
+
+(local nvim-ts-autotag (require :nvim-ts-autotag))
+(nvim-ts-autotag.setup)
 
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,5 @@
 -- [nfnl] Compiled from fnl/plugins/treesitter.fnl by https://github.com/Olical/nfnl, do not edit.
-local ts = require("nvim-treesitter.configs")
-return ts.setup({highlight = {enable = true}})
+local treesitter_context = require("treesitter-context")
+treesitter_context.setup()
+local nvim_ts_autotag = require("nvim-ts-autotag")
+return nvim_ts_autotag.setup()
