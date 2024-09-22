81638c316c72ce33d310b63a68dd55c60d0a43af
Drake Bott <me@drake.dev>
2024-09-22 17:22:02 -0500

    update

--- .config/nvim/lua/general-config.lua
+++ .config/nvim/lua/general-config.lua
@@ -1,8 +1,5 @@
 -- [nfnl] Compiled from fnl/general-config.fnl by https://github.com/Olical/nfnl, do not edit.
 vim.g.mapleader = " "
+vim.g.maplocalleader = ","
 vim.keymap.set("n", "<Leader>y", "\"+y", {desc = "Yank to clipboard"})
-vim.keymap.set("t", "<Esc>", "<C-\\\\><C-n>")
-vim.o.winwidth = 10
-vim.o.winminwidth = 10
-vim.o.equalalways = false
-return nil
+return vim.keymap.set("t", "<Esc>", "<C-\\\\><C-n>")
