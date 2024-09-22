db101cfd658b025b0d7b209c089df817ef9069de
Drake Bott <bottd@users.noreply.github.com>
2024-09-22 12:30:01 -0500

    update

--- .config/nvim/lua/general-config.lua
+++ .config/nvim/lua/general-config.lua
@@ -1,8 +1,4 @@
 -- [nfnl] Compiled from fnl/general-config.fnl by https://github.com/Olical/nfnl, do not edit.
-vim.api.nvim_command("set number")
-vim.api.nvim_command("set cursorline")
-vim.api.nvim_command("set smartindent")
-vim.o.guifont = "MonoLisa Nerd Font"
 vim.g.mapleader = " "
 vim.keymap.set("n", "<Leader>y", "\"+y", {desc = "Yank to clipboard"})
 vim.keymap.set("t", "<Esc>", "<C-\\\\><C-n>")
