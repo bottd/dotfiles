15ba813c2d42a4e23248e0650ccef359cc437bb4
Drake Bott <me@drake.dev>
2024-11-06 21:12:51 -0600

    update

--- .config/nvim/lua/plugins/care.lua
+++ .config/nvim/lua/plugins/care.lua
@@ -3,37 +3,48 @@ nvim_autopairs.setup()
 
 local care = require("care")
 care.setup({
-	ui = {
-		ghost_text = { position = "inline" },
-		menu = {
-			max_height = 10,
-			format_entry = function(entry, data)
-				local components = require("care.presets.components")
-				local completion_item = entry.completion_item
-				local entry_kind = type(completion_item.kind) == "string" and completion_item.kind
-					or require("care.utils.lsp").get_kind_name(completion_item.kind)
-				function SourceName()
-					return { { data.source_name, ("@care.type.fg.%s"):format(entry_kind) } }
-				end
-				return {
-					components.Label(entry, data, true),
-					SourceName(),
-					components.KindIcon(entry, "fg"),
-				}
-			end,
-		},
-	},
-	alignment = { "left", "right" },
-	selection_behavior = "insert",
-	confirm_behavior = "replace",
-	sorting_direction = "away-from-cursor",
-	sources = {
-		lsp = { max_entries = 5, priority = 1 },
-		cmp_buffer = { max_entries = 3 },
-	},
-	snippet_expansion = function(body)
-		require("luasnip").lsp_expand(body)
-	end,
+  ui = {
+    ghost_text = { position = "inline" },
+    menu = {
+      max_height = 10,
+      border = {
+        { "▄", "@care.menu" },
+        { "▄", "@care.menu" },
+        { "▄", "@care.menu" },
+        { "█", "@care.menu" },
+        { "▀", "@care.menu" },
+        { "▀", "@care.menu" },
+        { "▀", "@care.menu" },
+        { "█", "@care.menu" },
+      },
+      format_entry = function(entry, data)
+        local components = require("care.presets.components")
+        local completion_item = entry.completion_item
+        local entry_kind = type(completion_item.kind) == "string" and completion_item.kind
+            or require("care.utils.lsp").get_kind_name(completion_item.kind)
+        function SourceName()
+          return { { data.source_name, ("@care.type.fg.%s"):format(entry_kind) } }
+        end
+
+        return {
+          components.KindIcon(entry, "fg"),
+          components.Label(entry, data, true),
+          SourceName(),
+        }
+      end,
+    },
+  },
+  alignment = { "left", "right" },
+  selection_behavior = "insert",
+  confirm_behavior = "replace",
+  sorting_direction = "away-from-cursor",
+  sources = {
+    lsp = { max_entries = 5, priority = 1 },
+    cmp_buffer = { max_entries = 3 },
+  },
+  snippet_expansion = function(body)
+    require("luasnip").lsp_expand(body)
+  end,
 })
 
 vim.keymap.set("i", "<Cr>", "<Plug>(CareConfirm)")
