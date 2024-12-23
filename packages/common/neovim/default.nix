4e54e5a5ee5bfbf5ff4fbd047bd325964b6ef645
Drake Bott <bott.da@gmail.com>
2024-12-23 09:59:51 -0700

    Update

--- packages/common/neovim/default.nix
+++ packages/common/neovim/default.nix
@@ -1,24 +1,26 @@
-{pkgs, ...}: {
-  home.file.".config/nvim/after" = {
-    source = ./after;
-    recursive = true;
-  };
+{pkgs, config, lib, inputs, ...}: {
+  home.file = {
+    ".config/nvim/after" = {
+      source = ./after;
+      recursive = true;
+    };
 
-  home.file.".config/nvim/lua" = {
-    source = ./lua;
-    recursive = true;
-  };
+    ".config/nvim/lua" = {
+      source = ./lua;
+      recursive = true;
+    };
 
-  home.file.".config/nvim/dict.txt" = {
-    source = ./dict.txt;
-  };
+    ".config/nvim/dict.txt" = {
+      source = ./dict.txt;
+    };
 
-  home.file.".config/nvim/init.lua" = {
-    source = ./init.lua;
-  };
+    ".config/nvim/init.lua" = {
+      source = ./init.lua;
+    };
 
-  home.file.".config/nvim/rocks.toml" = {
-    source = ./rocks.toml;
+    ".config/nvim/rocks.toml" = {
+      source = ./rocks.toml;
+    };
   };
 
   programs.neovim.enable = true;
