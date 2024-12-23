fc1d23d17fea005e0f5670a5501eeef5fc86a69a
Drake Bott <bott.da@gmail.com>
2024-12-23 10:34:32 -0700

    It works

--- packages/common/neovim/default.nix
+++ packages/common/neovim/default.nix
@@ -1,25 +1,25 @@
 {pkgs, config, lib, inputs, ...}: {
   home.file = {
     ".config/nvim/after" = {
-      source = ./after;
+      source = config.lib.meta.createSymlink("packages/common/neovim/after");
       recursive = true;
     };
 
     ".config/nvim/lua" = {
-      source = ./lua;
+      source = config.lib.meta.createSymlink("packages/common/neovim/lua");
       recursive = true;
     };
 
     ".config/nvim/dict.txt" = {
-      source = config.lib.meta.createSymlink(./dict.txt);
+      source = config.lib.meta.createSymlink("packages/common/neovim/dict.txt");
     };
 
     ".config/nvim/init.lua" = {
-      source = config.lib.meta.createSymlink(./init.lua);
+      source = config.lib.meta.createSymlink("packages/common/neovim/init.lua");
     };
 
     ".config/nvim/rocks.toml" = {
-      source = config.lib.meta.createSymlink(./rocks.toml);
+      source = config.lib.meta.createSymlink("packages/common/neovim/rocks.toml");
     };
   };
 
