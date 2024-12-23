df9f1795fd7535373e6d76c0db159391622a29eb
Drake Bott <bottd@users.noreply.github.com>
2024-12-23 09:08:18 -0800

    Multi flake (#15)
    * create flakes dir
    
    * nested flakes
    
    * update flake root
    
    * remove old flake
    
    * Update, install blink
    
    * Rocks update
    
    * Update
    
    * Update
    
    * Update, map nvim config files
    
    * Update
    
    * Update

--- packages/common/neovim/default.nix
+++ packages/common/neovim/default.nix
@@ -1,7 +1,27 @@
-{pkgs, ...}: {
-  home.file.".config/nvim" = {
-    source = ../neovim;
-    target = "link";
+{pkgs, config, lib, inputs, ...}: {
+  home.file = {
+    ".config/nvim/after" = {
+      source = ./after;
+      recursive = true;
+    };
+
+    ".config/nvim/lua" = {
+      source = ./lua;
+      recursive = true;
+    };
+
+    ".config/nvim/dict.txt" = {
+      source = ./dict.txt;
+    };
+
+    ".config/nvim/init.lua" = {
+      source = ./init.lua;
+    };
+
+    ".config/nvim/rocks.toml" = {
+      source = config.lib.file.mkOutOfStoreSymlink("${config.home.homeDirectory}/.config/home-manager/packages/common/neovim/rocks.toml");
+    };
   };
+
   programs.neovim.enable = true;
 }
--- packages/common/neovim/lua/plugins/lsp.lua
+++ packages/common/neovim/lua/plugins/lsp.lua
@@ -2,97 +2,99 @@ local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 
 vim.keymap.set("n", "gl", function()
-	return vim.diagnostic.open_float()
+  return vim.diagnostic.open_float()
 end)
 
 vim.keymap.set("n", "[d", function()
-	return vim.diagnostic.goto_prev()
+  return vim.diagnostic.goto_prev()
 end)
 
 vim.keymap.set("n", "]d", function()
-	return vim.diagnostic.goto_next()
+  return vim.diagnostic.goto_next()
 end)
 
 vim.api.nvim_create_autocmd("LspAttach", {
-	desc = "LSP Actions",
-	callback = function(event)
-		local opts = { buffer = event.buf }
-		vim.keymap.set("n", "K", function()
-			return vim.lsp.buf.hover()
-		end, opts)
-		vim.keymap.set("n", "gd", function()
-			return vim.lsp.buf.definition()
-		end, opts)
-		vim.keymap.set("n", "gD", function()
-			return vim.lsp.buf.declaration()
-		end, opts)
-		vim.keymap.set("n", "gi", function()
-			return vim.lsp.buf.implementation()
-		end, opts)
-		vim.keymap.set("n", "go", function()
-			return vim.lsp.buf.type_definition()
-		end, opts)
-		vim.keymap.set("n", "gr", function()
-			return vim.lsp.buf.references()
-		end, opts)
-		vim.keymap.set("n", "gs", function()
-			return vim.lsp.buf.signature_help()
-		end, opts)
-		vim.keymap.set("n", "<F2>", function()
-			return vim.lsp.buf.rename()
-		end, opts)
-		return vim.keymap.set("n", "<F4>", function()
-			return vim.lsp.buf.code_action()
-		end, opts)
-	end,
+  desc = "LSP Actions",
+  callback = function(event)
+    local opts = { buffer = event.buf }
+    vim.keymap.set("n", "K", function()
+      return vim.lsp.buf.hover()
+    end, opts)
+    vim.keymap.set("n", "gd", function()
+      return vim.lsp.buf.definition()
+    end, opts)
+    vim.keymap.set("n", "gD", function()
+      return vim.lsp.buf.declaration()
+    end, opts)
+    vim.keymap.set("n", "gi", function()
+      return vim.lsp.buf.implementation()
+    end, opts)
+    vim.keymap.set("n", "go", function()
+      return vim.lsp.buf.type_definition()
+    end, opts)
+    vim.keymap.set("n", "gr", function()
+      return vim.lsp.buf.references()
+    end, opts)
+    vim.keymap.set("n", "gs", function()
+      return vim.lsp.buf.signature_help()
+    end, opts)
+    vim.keymap.set("n", "<F2>", function()
+      return vim.lsp.buf.rename()
+    end, opts)
+    return vim.keymap.set("n", "<F4>", function()
+      return vim.lsp.buf.code_action()
+    end, opts)
+  end,
 })
 
+local capabilities = require('blink.cmp').get_lsp_capabilities()
 mason.setup({})
 return mason_lspconfig.setup({
-	ensure_installed = {
-		"cssls",
-		"eslint",
-		"graphql",
-		"html",
-		"jsonnet_ls",
-		"rust_analyzer",
-		"sqlls",
-		"lua_ls",
-		"svelte",
-		"tailwindcss",
-		"ts_ls",
-		"harper_ls",
-	},
-	handlers = {
-		function(server_name)
-			local server = require("lspconfig")[server_name]
-			return server.setup({})
-		end,
-		harper_ls = function()
-			local server = require("lspconfig")["harper_ls"]
-			return server.setup({
-				filetypes = { "norg", "markdown" },
-				settings = { ["harper-ls"] = { userDictPath = "~/.config/nvim/dict.txt" } },
-			})
-		end,
-		rust_analyzer = function()
-			local server = require("lspconfig")["rust_analyzer"]
-			return server.setup({
-				cargo = { features = { "ssr" } },
-				procMacro = { ignored = { leptos_macro = { "server" } } },
-			})
-		end,
-		lua_ls = function()
-			local server = require("lspconfig")["lua_ls"]
-			return server.setup({
-				Lua = {
-					diagnostics = { globals = { "vim" } },
-					workspace = { checkThirdParty = false },
-					telemetry = {
-						enable = false,
-					},
-				},
-			})
-		end,
-	},
+  ensure_installed = {
+    "cssls",
+    "eslint",
+    "graphql",
+    "html",
+    "jsonnet_ls",
+    "rust_analyzer",
+    "sqlls",
+    "lua_ls",
+    "svelte",
+    "tailwindcss",
+    "ts_ls",
+    "harper_ls",
+  },
+  handlers = {
+    function(server_name)
+      local server = require("lspconfig")[server_name]
+      return server.setup({})
+    end,
+    harper_ls = function()
+      local server = require("lspconfig")["harper_ls"]
+      return server.setup({
+        filetypes = { "norg", "markdown" },
+        settings = { ["harper-ls"] = { userDictPath = "~/.config/nvim/dict.txt" } },
+      })
+    end,
+    rust_analyzer = function()
+      local server = require("lspconfig")["rust_analyzer"]
+      return server.setup({
+        cargo = { features = { "ssr" } },
+        procMacro = { ignored = { leptos_macro = { "server" } } },
+      })
+    end,
+    lua_ls = function()
+      local server = require("lspconfig")["lua_ls"]
+      return server.setup({
+        capabilities = capabilities,
+        Lua = {
+          diagnostics = { globals = { "vim" } },
+          workspace = { checkThirdParty = false },
+          telemetry = {
+            enable = false,
+          },
+        },
+      })
+    end,
+  },
 })
