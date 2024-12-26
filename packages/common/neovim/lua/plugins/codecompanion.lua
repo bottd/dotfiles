37620071d796ddd26a43cad7dff1bd0123ff8a83
Drake Bott <me@drake.dev>
2024-12-26 08:25:58 -0700

    Symlink (#17)
    * nfnl
    
    * Fix default workspace key
    
    * Fnl
    
    * Ignore compiled lua
    
    * Update
    
    * Update ignore
    
    * Remove auto-dark-mode plugin, use wezterm
    
    * Update dots, fix dark mode remove spell cmp source
    
    * Update nvim dots
    
    * Add headlines
    
    * Open journal floating window
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update dots, Neorg major update!
    
    * Add nu
    
    * Update
    
    * Update - neovide
    
    * Update
    
    * Update dots
    
    * update
    
    * Update, dashboard
    
    * Update, dashboard
    
    * Update, luarocks + neorg v8
    
    * Add bufdelete plugin for alpha on last buf closed
    
    * Update
    
    * Update
    
    * Update, remove Comment.nvim comment now in nightly
    
    * Update fzf build
    
    * Update
    
    * Add lazygit nvim
    
    * Update
    
    * WIP - migrate from lazy to rocks
    
    * Remove lazy lock
    
    * Update Rocks configs
    
    * Cleanup init
    
    * Remove yabai
    
    * Remove maplocalleader binding to space
    
    * Lsp
    
    * Hydra and git to rocks
    
    * Update
    
    * ts-comments.nvim
    
    * Update
    
    * Remove custom rosepine wezterm theme, use wezterm included rosepine
    
    * Update nvim
    
    * Add nix to nu env path
    
    * Bump rocks versions
    
    * Update nvim
    
    * Update rocks
    
    * Update rocks
    
    * Add local.env.nu for machine vars
    
    * Update rocks
    
    * Disable luarocks ts parser installations, running :TSInstall all as temp fix
    
    * Update rocks
    
    * Update rocks
    
    * CXX flags
    
    * Add luarocks config for mac arch
    
    * Update rocks
    
    * Remove autotag ts setup
    
    * udate rocks
    
    * use legacy ts from luarocks
    
    * Update rocks
    
    * Update, which-key v3
    
    * Update rocks
    
    * Update -- install my first plugin!
    
    * Upcase neorg workspace names
    
    * Update
    
    * Update
    
    * Update
    
    * Add conform, format config
    
    * Conform
    
    * Catppuccin
    
    * Update, move nushell to .config
    
    * Remove tmux
    
    * Nushell default wezterm shell
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Track karabiner.json
    
    * Update wezterm/sketchybar
    
    * Update
    
    * mini indentscope
    
    * Update
    
    * Care
    
    * update
    
    * care
    
    * update
    
    * Update
    
    * update
    
    * Update
    
    * update
    
    * update
    
    * Update
    
    * Update
    
    * RIP fennel
    
    * update
    
    * update
    
    * use lua for wezterm
    
    * use lua for wezterm
    
    * cross platform wezterm config
    
    * remove wezterm from yadm
    
    * update
    
    * switch from YADM to nix for config
    
    * move nvim and wezterm config files
    
    * shorten path
    
    * Add karabiner config
    
    * Add nushell, starship conf
    
    * Create flakes dir
    
    * create flakes dir
    
    * nested flakes
    
    * update flake root
    
    * remove old flake
    
    * Multi flake (#15)
    
    * create flakes dir
    
    * nested flakes
    
    * update flake root
    
    * remove old flake
    
    * Update, install blink
    
    * Rocks update
    
    * Update
    
    * Update
    
    * Update, map nvim config files
    
    * Update
    
    * Update
    
    * Add util for sym link
    
    * Update
    
    * It works
    
    * Update
    
    * Code companion bindings
    
    * Update flake
    
    * Remove old api key
    
    * Add git repo filter
    
    * enable mini.ai
    
    ---------
    
    Co-authored-by: Drake Bott <dbott@irisidentityprotection.com>

---
+++ packages/common/neovim/lua/plugins/codecompanion.lua
@@ -0,0 +1,25 @@
+require('codecompanion').setup({
+  strategies = {
+    chat = {
+      adapter = "anthropic"
+    }
+  },
+  adapters = {
+    anthropic = function()
+      return require("codecompanion.adapters").extend("anthropic", {
+        env = {
+          -- TODO: Set api key in env, I just leaked my last api key :o
+          api_key = os.getenv("ANTHROPIC_API_KEY")
+        },
+      })
+    end,
+  },
+})
+
+vim.keymap.set("n", "<leader>ac", ":CodeCompanionChat", { desc = "Chat" })
+vim.keymap.set("n", "<leader>ad", ":CodeCompanionCommand", { desc = "Command" })
+vim.keymap.set("n", "<leader>aa", ":CodeCompanionActions", { desc = "Actions" })
+
+local which_key = require("which-key")
+
+which_key.add({ { "<leader>a", desc = "AI" } })
