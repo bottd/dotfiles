2415b9edc000b0e35a282373519688fce6759769
Drake Bott <bottd@users.noreply.github.com>
2025-01-08 18:13:13 -0600

    Symlink aerospace config

--- packages/darwin/aerospace/default.nix
+++ packages/darwin/aerospace/default.nix
@@ -1,7 +1,7 @@
 
-{pkgs, ...}: {
+{pkgs, config, ...}: {
   home.file.".aerospace.toml" = {
-    source = ./aerospace.toml;
+    source = config.lib.meta.createSymlink("packages/darwin/aerospace/aerospace.toml");
   };
 
   home.packages = with pkgs; [ 
