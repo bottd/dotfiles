851d209e59f959f11ac5d29e81c0b6f1e2397c90
Drake Bott <bott.da@gmail.com>
2025-01-10 14:01:13 -0600

    Update desktop flake

--- flakes/desktop/flake.nix
+++ flakes/desktop/flake.nix
@@ -1,12 +1,11 @@
 {
   description = "Drake - Desktop";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-    }
+    };
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
@@ -15,27 +14,30 @@
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.drakeb = import ../../home.nix;
-
-              # Optionally, use home-manager.extraSpecialArgs to pass
-              # arguments to home.nix
-              extraSpecialArgs = {
-                username = "drakeb";
-                root = ../../.;
-                neorgWorkspace = "chalet";
-              };
-            }
-          }
-          ../../util/default.nix
-          ../../home.nix
-          ../../packages/common/default.nix
         ];
       };
+    };
+    packages.x86_64-linux = {
+      homeConfigurations = {
+        drakeb = home-manager.nixosModules.home-manager {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.drakeb = import ../../home.nix;
+
+            extraSpecialArgs = {
+              username = "drakeb";
+              root = ../../.;
+              neorgWorkspace = "chalet";
+            };
+          };
+          modules = [
+            ../../util/default.nix
+              ../../home.nix
+              ../../packages/common/default.nix
+          ];
+        }
+      }
     };
   };
 }
