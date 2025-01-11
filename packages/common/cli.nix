4966d7cc315b5802fe133034eba66093d29acaa9
installer <installer@gmail.com>
2025-01-11 13:16:01 -0600

    update

--- packages/common/cli.nix
+++ packages/common/cli.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
+    gcc
     # Utility for filtering git repo history, useful for cleaning accidentally committed secrets
     git-filter-repo
     gh
