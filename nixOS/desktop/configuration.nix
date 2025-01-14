0385b62ad4c44937dd6a3b39c2479bb0ed35fdf6
installer <installer@gmail.com>
2025-01-14 14:44:28 -0600

    Single flake

--- nixOS/desktop/configuration.nix
+++ nixOS/desktop/configuration.nix
@@ -84,12 +84,6 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
-  nix = {
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
   # hardware.pulseaudio.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
