3859e393529ef169f1820c3965ca94e8aff9ed89
Drake Bott <bott.da@gmail.com>
2025-01-14 11:35:45 -0600

    WIP - root flake working on darwin

--- flake.nix
+++ flake.nix
@@ -1,24 +1,25 @@
 {
   description = "Drake Flake";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:nixos/nixos-hardware/master";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     mac-app-util.url = "github:hraban/mac-app-util";
 
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    }
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
   };
 
-  outputs = inputs@{ self, flake-parts, home-manager, mac-app-util, nixos-hardware, nixpkgs, ... }: {
+  outputs = inputs@{ self, home-manager, mac-app-util, nixos-hardware, nixpkgs, ... }: {
     # sudo nixos-rebuild switch --flake .#desktop
     nixosConfigurations = {
       desktop = import ./nixOS/desktop.nix {inherit inputs;};
     };
     # home-manager switch --flake .#drakebott
     packages.aarch64-darwin.homeConfigurations = {
-      drakebott = import ./darwin/personal.nix {inherit inputs;};
+      drakebott = import ./darwin/personal.nix {inherit home-manager inputs mac-app-util nixpkgs;};
     };
   };
 }
