3ef8cf30ea27b35d4da146255a1cbe6ec5d13f9d
installer <installer@gmail.com>
2025-01-15 19:55:37 -0600

    Update
    Update

--- home/common/cli.nix
+++ home/common/cli.nix
@@ -5,11 +5,8 @@
     git
     # Utility for filtering git repo history, useful for cleaning accidentally committed secrets
     git-filter-repo
-<<<<<<< HEAD:home/common/cli.nix
     gh
     jujutsu
-=======
->>>>>>> d8fab94 (Update):packages/common/cli.nix
 
     gnumake
 
