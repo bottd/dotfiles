f7dd23458a5c5f10590d0da5ad977d27c455679a
Drake Bott <bottd@users.noreply.github.com>
2025-02-01 09:27:29 -0600

    Update

--- darwin/macbook/default.nix
+++ darwin/macbook/default.nix
@@ -1,30 +1,36 @@
-{home-manager, mac-app-util, nix-darwin, nixpkgs, ...}:
-  nix-darwin.lib.darwinSystem {
-    pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-    system = "aarch64-darwin";
-    modules = [
-      mac-app-util.darwinModules.default
-        ./configuration.nix
-        home-manager.darwinModules.home-manager {
-          users.users.drakebott.home = "/Users/drakebott";
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            extraSpecialArgs = {
-              username = "drakebott";
-              root = ./../..;
-              neorgWorkspace = "chalet";
-            };
-            users.drakebott = {
-              imports = [
-                mac-app-util.homeManagerModules.default
-                  ../../util/default.nix
-                  ../../home.nix
-                  ../../home/darwin/default.nix
-                  ../../home/common/default.nix
-              ];
-            };
+{home-manager, mac-app-util, nix-darwin, nixpkgs, ...}@inputs:
+nix-darwin.lib.darwinSystem {
+  pkgs = import nixpkgs { 
+    system = "aarch64-darwin"; 
+    config.allowUnfree = true; 
+  };
+  specialArgs = {
+    inherit inputs;
+  };
+  system = "aarch64-darwin";
+  modules = [
+    mac-app-util.darwinModules.default
+      ./configuration.nix
+      home-manager.darwinModules.home-manager {
+        users.users.drakebott.home = "/Users/drakebott";
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          extraSpecialArgs = {
+            username = "drakebott";
+            root = ./../..;
+            neorgWorkspace = "chalet";
           };
-        }
-    ];
-  }
+          users.drakebott = {...}: {
+            imports = [
+              mac-app-util.homeManagerModules.default
+                ../../util/default.nix
+                ../../home.nix
+                ../../home/darwin/default.nix
+                ../../home/common/default.nix
+            ];
+          };
+        };
+      }
+  ];
+}
