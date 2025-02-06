f8cb8d059ae51f3cd690591331d65999e14c9c29
Drake Bott <bott.da@gmail.com>
2025-02-06 17:11:52 -0600

    Create libiconv darwin module

--- home/common/neovim/lua/plugins/blink.lua
+++ home/common/neovim/lua/plugins/blink.lua
@@ -1,67 +1,68 @@
 require("blink.cmp").setup({
-  keymap = { preset = "super-tab" },
-  snippets = { preset = 'luasnip' },
-  sources = {
-    default = { 'lsp', 'path', 'snippets', 'buffer', 'codecompanion' },
-    providers = {
-      codecompanion = {
-        name = "CodeCompanion",
-        module = "codecompanion.providers.completion.blink",
-        enabled = true,
-      },
-    },
-  },
-  completion = {
-    keyword = {
-      range = 'prefix',
-    },
-    list = {
-      selection = {
-        preselect = true,
-        auto_insert = true
-      },
-      cycle = {
-        from_bottom = true,
-        from_top = true,
-      },
-    },
-    accept = {
-      create_undo_point = true,
-      auto_brackets = {
-        enabled = true,
-        kind_resolution = {
-          enabled = true,
-        },
-        semantic_token_resolution = {
-          enabled = true,
-        },
-      },
-    },
+	keymap = { preset = "super-tab" },
+	snippets = { preset = "luasnip" },
+	sources = {
+		-- default = { "lsp", "path", "snippets", "buffer", "codecompanion" },
+		default = { "codecompanion" },
+		providers = {
+			codecompanion = {
+				name = "CodeCompanion",
+				module = "codecompanion.providers.completion.blink",
+				enabled = true,
+			},
+		},
+	},
+	completion = {
+		keyword = {
+			range = "prefix",
+		},
+		list = {
+			selection = {
+				preselect = true,
+				auto_insert = true,
+			},
+			cycle = {
+				from_bottom = true,
+				from_top = true,
+			},
+		},
+		accept = {
+			create_undo_point = true,
+			auto_brackets = {
+				enabled = true,
+				kind_resolution = {
+					enabled = true,
+				},
+				semantic_token_resolution = {
+					enabled = true,
+				},
+			},
+		},
 
-    menu = {
-      border = 'padded',
-      draw = {
-        padding = 1,
-        gap = 1,
-        treesitter = { 'lsp' },
-        columns = {
-          { 'kind_icon' },
-          { 'label',      'label_description', gap = 1 },
-          { 'source_name' }
-        },
-      },
-    },
+		menu = {
+			border = "padded",
+			draw = {
+				padding = 1,
+				gap = 1,
+				treesitter = { "lsp" },
+				columns = {
+					{ "kind_icon" },
+					{ "label", "label_description", gap = 1 },
+					{ "source_name" },
+				},
+			},
+		},
 
-    documentation = {
-      auto_show = true,
-      window = {
-        border = 'padded',
-        scrollbar = true,
-      },
-    },
+		documentation = {
+			auto_show = true,
+			window = {
+				border = "padded",
+				scrollbar = true,
+			},
+		},
 
-    ghost_text = {
-      enabled = false,
-    },
-  }
+		ghost_text = {
+			enabled = false,
+		},
+	},
 })
---
+++ home/darwin/libiconv.nix
@@ -0,0 +1,10 @@
+{pkgs, config, ...}: {
+  home.packages = with pkgs; [
+    libiconv
+  ];
+
+  home.sessionVariables = {
+    LIBRARY_PATH = "${pkgs.libiconv}/lib";
+    CPATH = "${pkgs.libiconv}/include";
+  };
+}
