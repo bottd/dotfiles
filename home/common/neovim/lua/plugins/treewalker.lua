d53883d941395a609a5691a6b6c747de94a00fb7
Drake Bott <bott.da@gmail.com>
2025-02-16 08:30:00 -0600

    Update - fix trewalker conf

--- home/common/neovim/lua/plugins/treewalker.lua
+++ home/common/neovim/lua/plugins/treewalker.lua
@@ -1,17 +1,19 @@
-require("treewalker").setup()
+require("treewalker").setup({})
 
-vim.keymap.set("n", "<leader>th", ":Treewalker Left", { desc = "Move Left" })
-vim.keymap.set("n", "<leader>tj", ":Treewalker Down", { desc = "Move Down" })
-vim.keymap.set("n", "<leader>tk", ":Treewalker Up", { desc = "Move Up" })
-vim.keymap.set("n", "<leader>tl", ":Treewalker Right", { desc = "Move Right" })
+vim.keymap.set("n", "<leader>t", function()
+	require("which-key").show({
+		keys = "<leader>t",
+		group = "Treewalker",
+		loop = true,
+	})
+end, { desc = "Treewalker" })
 
-vim.keymap.set("n", "<leader>tk", ":Treewalker SwapLeft", { desc = "Swap Left" })
-vim.keymap.set("n", "<leader>tk", ":Treewalker SwapDown", { desc = "Swap Down" })
-vim.keymap.set("n", "<leader>tk", ":Treewalker SwapUp", { desc = "Swap Up" })
-vim.keymap.set("n", "<leader>tk", ":Treewalker SwapRight", { desc = "Swap Right" })
+vim.keymap.set("n", "<leader>th", ":Treewalker Left<Cr>", { desc = "Move Left" })
+vim.keymap.set("n", "<leader>tj", ":Treewalker Down<Cr>", { desc = "Move Down" })
+vim.keymap.set("n", "<leader>tk", ":Treewalker Up<Cr>", { desc = "Move Up" })
+vim.keymap.set("n", "<leader>tl", ":Treewalker Right<Cr>", { desc = "Move Right" })
 
-require("which-key").show({
-	keys = "<leader>t",
-	group = "Treewalker",
-	loop = true,
-})
+vim.keymap.set("n", "<leader>tH", ":Treewalker SwapLeft<Cr>", { desc = "Swap Left" })
+vim.keymap.set("n", "<leader>tJ", ":Treewalker SwapDown<Cr>", { desc = "Swap Down" })
+vim.keymap.set("n", "<leader>tK", ":Treewalker SwapUp<Cr>", { desc = "Swap Up" })
+vim.keymap.set("n", "<leader>tL", ":Treewalker SwapRight<Cr>", { desc = "Swap Right" })
