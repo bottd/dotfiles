8bf5dd69d23c3bb9ab96f69401d63e63b32ca51c
Drake Bott <bottd@users.noreply.github.com>
2025-02-16 11:55:36 -0600

    Update

--- home/common/neovim/lua/plugins/snacks.lua
+++ home/common/neovim/lua/plugins/snacks.lua
@@ -1,53 +1,53 @@
 local snacks = require("snacks")
 snacks.setup({
-  bigfile = { enabled = true },
-  dashboard = {
-    enabled = true,
-    preset = {
-      pick = "telsecope.nvim",
-    },
-    sections = {
-      {
-        section = "terminal",
-        cmd = "fortune -s | cowsay -f stegosaurus",
-        hl = "header",
-        height = 24,
-        width = 68
-      },
-      { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-      { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-    }
-  },
-  gitbrowse = { enabled = true },
-  lazygit = { enabled = true },
-  styles = { zen = { backdrop = { transparent = false } } },
-  zen = { enabled = true }
+	bigfile = { enabled = true },
+	dashboard = {
+		enabled = true,
+		preset = {
+			pick = "telsecope.nvim",
+		},
+		sections = {
+			{
+				section = "terminal",
+				cmd = "fortune -s | cowsay -f stegosaurus",
+				hl = "header",
+				height = 24,
+				width = 68,
+			},
+			{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+			{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+		},
+	},
+	gitbrowse = { enabled = true },
+	lazygit = { enabled = true },
+	styles = { zen = { backdrop = { transparent = false } } },
+	zen = { enabled = true },
 })
 
 local wk = require("which-key")
 wk.add({
-  {
-    "<leader>gg",
-    function()
-      Snacks.lazygit()
-    end,
-    desc = "Lazygit",
-  },
+	{
+		"<leader>gg",
+		function()
+			Snacks.lazygit()
+		end,
+		desc = "Lazygit",
+	},
 
-  {
-    "<leader>gw",
-    function()
-      Snacks.gitbrowse()
-    end,
-    desc = "Open in browser",
-    icon = " "
-  },
-  {
-    "<leader>wz",
-    function()
-      Snacks.zen()
-    end,
-    desc = "Zen mode",
-    icon = "󱅻 "
-  }
+	{
+		"<leader>gw",
+		function()
+			Snacks.gitbrowse()
+		end,
+		desc = "Open in browser",
+		icon = " ",
+	},
+	{
+		"<leader>wz",
+		function()
+			Snacks.zen()
+		end,
+		desc = "Zen mode",
+		icon = "󱅻 ",
+	},
 })
--- home/common/neovim/lua/plugins/treewalker.lua
+++ home/common/neovim/lua/plugins/treewalker.lua
@@ -1,19 +1,25 @@
 require("treewalker").setup({})
+local wk = require("which-key")
 
-vim.keymap.set("n", "<leader>t", function()
-	require("which-key").show({
-		keys = "<leader>t",
+wk.add({
+	{
+		"<leader>t",
+		function()
+			wk.show({
+				keys = "<leader>t",
+				loop = true,
+			})
+		end,
 		group = "Treewalker",
-		loop = true,
-	})
-end, { desc = "Treewalker" })
-
-vim.keymap.set("n", "<leader>th", ":Treewalker Left<Cr>", { desc = "Move Left" })
-vim.keymap.set("n", "<leader>tj", ":Treewalker Down<Cr>", { desc = "Move Down" })
-vim.keymap.set("n", "<leader>tk", ":Treewalker Up<Cr>", { desc = "Move Up" })
-vim.keymap.set("n", "<leader>tl", ":Treewalker Right<Cr>", { desc = "Move Right" })
-
-vim.keymap.set("n", "<leader>tH", ":Treewalker SwapLeft<Cr>", { desc = "Swap Left" })
-vim.keymap.set("n", "<leader>tJ", ":Treewalker SwapDown<Cr>", { desc = "Swap Down" })
-vim.keymap.set("n", "<leader>tK", ":Treewalker SwapUp<Cr>", { desc = "Swap Up" })
-vim.keymap.set("n", "<leader>tL", ":Treewalker SwapRight<Cr>", { desc = "Swap Right" })
+		icon = "󰹩",
+	},
+	{ "<leader>th", ":Treewalker Left<Cr>", desc = "Move Left" },
+	{ "<leader>th", ":Treewalker Left<Cr>", desc = "Move Left" },
+	{ "<leader>tj", ":Treewalker Down<Cr>", desc = "Move Down" },
+	{ "<leader>tk", ":Treewalker Up<Cr>", desc = "Move Up" },
+	{ "<leader>tl", ":Treewalker Right<Cr>", desc = "Move Right" },
+	{ "<leader>tH", ":Treewalker SwapLeft<Cr>", desc = "Swap Left" },
+	{ "<leader>tJ", ":Treewalker SwapDown<Cr>", desc = "Swap Down" },
+	{ "<leader>tK", ":Treewalker SwapUp<Cr>", desc = "Swap Up" },
+	{ "<leader>tL", ":Treewalker SwapRight<Cr>", desc = "Swap Right" },
+})
