874abb82145ca7d03e042349ab881bd623432c4f
Drake Bott <bottd@users.noreply.github.com>
2025-02-24 13:40:59 -0600

    group unfree programs

--- home/common/unfree.nix
+++ home/common/unfree.nix
@@ -1,10 +1,21 @@
-#INFO: On mac, since this modifies signed discord, it now has to be allowed in security settings.
-{pkgs, ...}: {
+{
+  pkgs,
+  nixpkgs,
+  ...
+}: {
   home.packages = with pkgs; [
     (discord.override {
       # withOpenASAR = true;
       # Vencord produces an unopenable discord client on mac for me
       # withVencord = true;
     })
+
+    spotify
   ];
+
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (pkgs.lib.getName pkg) [
+      "spotify"
+      "discord"
+    ];
 }
