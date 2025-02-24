d155bc32b3af2ed9f9172ffdd07bb08e6cf48c4b
Drake Bott <bottd@users.noreply.github.com>
2025-02-24 14:53:27 -0600

    install discord on linux only

--- home/common/unfree.nix
+++ home/common/unfree.nix
@@ -6,17 +6,10 @@
 }: {
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
-      "discord"
       "spotify"
     ];
 
   home.packages = with pkgs; [
-    (discord.override {
-      # withOpenASAR = true;
-      # Vencord produces an unopenable discord client on mac for me
-      # withVencord = true;
-    })
-
     spotify
   ];
 }
