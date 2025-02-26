8037bc02b4d0a92ad5ee2a039115301c5547e2f5
Drake Bott <bott.da@gmail.com>
2025-02-25 19:13:48 -0600

    android studio

--- home/linux/unfree.nix
+++ home/linux/unfree.nix
@@ -6,11 +6,13 @@
 }: {
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
+      "android-studio"
       "discord"
       "steam"
     ];
 
   home.packages = with pkgs; [
+    android-studio
     (discord.override {
       # withOpenASAR = true;
       # Vencord produces an unopenable discord client on mac for me
