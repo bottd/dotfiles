c8baebf54c0cc56af0ed0302238800c25a47bb98
Drake Bott <me@drake.dev>
2025-03-01 12:36:19 -0600

    FIX ROCKS ON LINUX

--- home/common/neovim/default.nix
+++ home/common/neovim/default.nix
@@ -21,32 +21,48 @@
     };
 
     ".config/nvim/init.lua" = {
-      source = config.lib.meta.createSymlink "home/common/neovim/init.lua";
+      text =
+        #lua
+        ''
+                      local rocks_config = {
+                        rocks_path = vim.fn.stdpath("data") .. "/rocks",
+                        luarocks_binary = "${pkgs.lua51Packages.luarocks}/bin/luarocks",
+                      }
+
+                    vim.g.rocks_nvim = rocks_config
+
+                      local luarocks_path = {
+                        vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
+                        vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
+                      }
+                    package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
+
+                      local luarocks_cpath = {
+                        vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
+                        vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
+                      }
+                    package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
+
+                      if not vim.uv.fs_stat(rocks_config.rocks_path) then
+                        vim.system({
+                            rocks_config.luarocks_binary,
+                            "install",
+                            "--tree",
+                            rocks_config.rocks_path,
+                            "--server=\'https://nvim-neorocks.github.io/rocks-binaries/\'",
+                            "--lua-version=5.1",
+                            "rocks.nvim",
+                            }):wait()
+                               end
+
+                                 vim.opt.runtimepath:append(vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*"))
+
+          require("general-config")
+        '';
     };
 
     ".config/nvim/rocks.toml" = {
       source = config.lib.meta.createSymlink "home/common/neovim/rocks.toml";
-    };
-    ".local/share/nvim/rocks/luarocks-config.lua" = {
-      enable = true;
-      text =
-        #lua
-        ''
-          lua_version = "5.1"
-          rocks_trees = { {
-            name = "rocks.nvim",
-            root = "${config.xdg.configHome}/.local/share/nvim/rocks"
-          } }
-
-          variables = {
-            LUA_INCDIR = "${pkgs.lua5_1}/include"
-          }
-          arch = ${
-            if pkgs.stdenv.hostPlatform.isDarwin
-            then "macosx-aarch64"
-            else "linux-x86_64"
-          }
-        '';
     };
   };
 
