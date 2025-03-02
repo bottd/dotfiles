5a1578ad9247f5cf20d54c4e1161efa73b8b1133
Drake Bott <bottd@users.noreply.github.com>
2025-03-02 14:45:20 -0600

    Update, remove unused nvim plugins

--- home/common/neovim/lua/plugins/telescope.lua
+++ home/common/neovim/lua/plugins/telescope.lua
@@ -11,19 +11,16 @@ telescope.setup({
 			override_file_sorter = true,
 			case_mode = "smart_case",
 		},
-		undo = { use_delta = true },
 	},
 })
 
 telescope.load_extension("fzf")
-telescope.load_extension("undo")
 
 vim.keymap.set("n", "<leader>fb", ":Telescope buffers<Cr>", { desc = "Find Buffer" })
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<Cr>", { desc = "Find Files" })
 vim.keymap.set("n", "<leader>fs", ":Telescope live_grep<Cr>", { desc = "Find String" })
 vim.keymap.set("n", "<leader>fk", ":Telescope keymaps<Cr>", { desc = "Find Keymap" })
 vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<Cr>", { desc = "Find Help" })
-vim.keymap.set("n", "<leader>fu", ":Telescope undo<Cr>", { desc = "Undo Tree" })
 
 local which_key = require("which-key")
 which_key.add({ { "<leader>f", desc = "Find" } })
