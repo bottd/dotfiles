8cb33fa4e40e5e9e596ef740e5739acd15edcb4c
Drake Bott <bottd@users.noreply.github.com>
2025-03-15 07:47:50 -0500

    Hypr in progress

--- nixOS/pocket/default.nix
+++ nixOS/pocket/default.nix
@@ -18,6 +18,7 @@ nixpkgs.lib.nixosSystem {
           ../../home.nix
           ../../util/default.nix
           ../../home/linux/default.nix
+          ../../home/linux/hyprland.nix
           ../../home/common/default.nix
           ../../home/opt/finance.nix
         ];
