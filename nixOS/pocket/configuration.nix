0966b830b3e6e13a4d62f0634e615e2b12ce8087
Drake Bott <bottd@users.noreply.github.com>
2025-03-15 07:49:37 -0500

    Disable hyprland temp

--- nixOS/pocket/configuration.nix
+++ nixOS/pocket/configuration.nix
@@ -83,6 +83,7 @@
 
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   hardware.graphics = {
