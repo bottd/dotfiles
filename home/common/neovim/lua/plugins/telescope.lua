7320dac2a80f4198a10640a71462602ac9a2e3f3
Drake Bott <bottd@users.noreply.github.com>
2025-03-20 11:19:02 -0500

    Show hidden files in telescope

--- home/common/neovim/lua/plugins/telescope.lua
+++ home/common/neovim/lua/plugins/telescope.lua
@@ -1,8 +1,19 @@
 local telescope = require("telescope")
 telescope.setup({
 	pickers = {
-		find_files = { layout_strategy = "vertical" },
-		live_grep = { layout_strategy = "vertical" },
+		find_files = {
+			layout_strategy = "vertical",
+			additional_args = function()
+				return { "--hidden" }
+			end,
+		},
+		live_grep = {
+			layout_strategy = "vertical",
+
+			additional_args = function()
+				return { "--hidden" }
+			end,
+		},
 	},
 	extensions = {
 		fzf = {
@@ -17,10 +28,24 @@ telescope.setup({
 telescope.load_extension("fzf")
 
 vim.keymap.set("n", "<leader>fb", ":Telescope buffers<Cr>", { desc = "Find Buffer" })
-vim.keymap.set("n", "<leader>ff", ":Telescope find_files<Cr>", { desc = "Find Files" })
-vim.keymap.set("n", "<leader>fs", ":Telescope live_grep<Cr>", { desc = "Find String" })
 vim.keymap.set("n", "<leader>fk", ":Telescope keymaps<Cr>", { desc = "Find Keymap" })
 vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<Cr>", { desc = "Find Help" })
 
-local which_key = require("which-key")
-which_key.add({ { "<leader>f", desc = "Find" } })
+local wk = require("which-key")
+wk.add({
+	{ "<leader>f", desc = "Find" },
+	{
+		"<leader>ff",
+		function()
+			require("telescope.builtin").find_files({ hidden = true })
+		end,
+		desc = "Find Files",
+	},
+	{
+		"<leader>fs",
+		function()
+			require("telescope.builtin").live_grep({ hidden = true })
+		end,
+		desc = "Find String",
+	},
+})
