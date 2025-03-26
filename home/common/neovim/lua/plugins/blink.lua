3b554387db0aa2c147d859646f4dfefc5df13c64
installer <installer@gmail.com>
2025-03-25 20:05:45 -0500

    Update, blink 1.0

--- home/common/neovim/lua/plugins/blink.lua
+++ home/common/neovim/lua/plugins/blink.lua
@@ -7,7 +7,7 @@ require("blink.cmp").setup({
 	keymap = { preset = "super-tab" },
 	snippets = { preset = "luasnip" },
 	sources = {
-		default = { "lsp", "path", "snippets", "buffer", "codecompanion", "copilot" },
+		default = { "lsp", "path", "snippets", "buffer", "codecompanion", "copilot", "omni", "cmdline" },
 		providers = {
 			codecompanion = {
 				name = "CodeCompanion",
@@ -20,53 +20,32 @@ require("blink.cmp").setup({
 			},
 		},
 	},
+	signature = { enabled = true },
 	completion = {
 		keyword = {
 			range = "prefix",
 		},
+
 		list = {
 			selection = {
 				preselect = true,
 				auto_insert = true,
 			},
-			cycle = {
-				from_bottom = true,
-				from_top = true,
-			},
-		},
-		accept = {
-			create_undo_point = true,
-			auto_brackets = {
-				enabled = true,
-				kind_resolution = {
-					enabled = true,
-				},
-				semantic_token_resolution = {
-					enabled = true,
-				},
-			},
 		},
 
 		menu = {
-			border = "padded",
 			draw = {
-				padding = 1,
-				gap = 1,
 				treesitter = { "lsp" },
 				columns = {
-					{ "kind_icon" },
-					{ "label", "label_description", gap = 1 },
 					{ "source_name" },
+					{ "kind_icon" },
+					{ "kind" },
+					{ "label", "label_description", gap = 1 },
 				},
 			},
 		},
 		documentation = {
 			auto_show = true,
-			auto_show_delay_ms = 500,
-			window = {
-				border = "padded",
-				scrollbar = true,
-			},
 		},
 		ghost_text = {
 			enabled = true,
@@ -76,5 +55,15 @@ require("blink.cmp").setup({
 		kind_icons = {
 			Copilot = "",
 		},
+	},
+	fuzzy = {
+		sorts = {
+			"exact",
+			"score",
+			"sort_text",
+		},
+	},
+	term = {
+		enabled = true,
 	},
 })
