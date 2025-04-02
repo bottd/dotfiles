210850b75cf62e1d9f48cf6a22cb2181decab070
Drake Bott <me@drake.dev>
2025-04-02 17:09:18 -0500

    Add zen

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
     ghostty = {
       url = "github:ghostty-org/ghostty";
     };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,7 +17,11 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:nixos/nixos-hardware/master";
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {...} @ inputs: rec {
