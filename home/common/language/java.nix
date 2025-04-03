213db4e797fc6052ad3b614d74828aa2066af35f
Drake Bott <bottd@users.noreply.github.com>
2025-04-02 22:57:19 -0500

    Java

--- home/common/language/java.nix
+++ home/common/language/java.nix
@@ -1,5 +1,8 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     jdt-language-server
+    vscode-extensions.vscjava.vscode-java-test
+    vscode-extensions.vscjava.vscode-java-debug
+    vscode-extensions.vscjava.vscode-gradle
   ];
 }
