6efc507ace754be73eb7d6d76dacb70b236ec17f
Drake Bott <bottd@users.noreply.github.com>
2025-04-15 14:02:41 -0500

    Vscode

--- home/common/unfree.nix
+++ home/common/unfree.nix
@@ -7,9 +7,11 @@
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
       "spotify"
+      "code-cursor"
     ];
 
   home.packages = with pkgs; [
     spotify
+    code-cursor
   ];
 }
