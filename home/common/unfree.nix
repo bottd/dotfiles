786ed936cdc499c7e9d0eb1a015273a7bb7ac5ce
Drake Bott <bottd@users.noreply.github.com>
2025-04-15 14:35:42 -0500

    Update

--- home/common/unfree.nix
+++ home/common/unfree.nix
@@ -7,11 +7,9 @@
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
       "spotify"
-      "code-cursor"
     ];
 
   home.packages = with pkgs; [
     spotify
-    code-cursor
   ];
 }
