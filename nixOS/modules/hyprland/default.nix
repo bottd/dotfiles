274b4e4fe78345ffaeb5447d9056959c62aaefd8
installer <installer@gmail.com>
2025-04-22 10:42:24 -0500

    hyprland?

--- nixOS/modules/hyprland/default.nix
+++ nixOS/modules/hyprland/default.nix
@@ -1,8 +1,28 @@
-{}: {
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
   programs.hyprland = {
     enable = true;
+    package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+
     withUWSM = true;
+    xwayland.enable = true;
   };
   programs.hyprlock.enable = true;
   programs.uwsm.enable = true;
+
+  environment.systemPackages = [
+  ];
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-gtk];
+  };
+
+  hardware = {
+    opengl.enable = true;
+  };
 }
