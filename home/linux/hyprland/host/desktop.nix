ba8db75653f419fa96f7a3cce9bd334fc8519f60
installer <installer@gmail.com>
2025-05-01 23:11:30 -0500

    Update

--- home/linux/hyprland/host/desktop.nix
+++ home/linux/hyprland/host/desktop.nix
@@ -6,8 +6,8 @@
 }: {
   wayland.windowManager.hyprland = {
     extraConfig = ''
-      monitor = DP-1, 2560x1440, 2560x0, 1, transform, 1
-      monitor = DP-3, 2560x1440, 0x0, 1
+      monitor = DP-3, 2560x1440, 2560x0, 1, transform, 3
+      monitor = DP-1, 2560x1440, 0x0, 1
     '';
   };
 }
