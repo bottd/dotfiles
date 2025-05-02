95f76e105440b1aeb1550f5d686d7bdeddb8ac77
installer <installer@gmail.com>
2025-05-02 14:15:28 -0500

    Update, set refresh rate

--- home/linux/hyprland/host/desktop.nix
+++ home/linux/hyprland/host/desktop.nix
@@ -7,7 +7,7 @@
   wayland.windowManager.hyprland = {
     extraConfig = ''
       monitor = DP-3, 2560x1440, 2560x0, 1, transform, 3
-      monitor = DP-1, 2560x1440, 0x0, 1
+      monitor = DP-1, 2560x1440@170, 0x0, 1
     '';
   };
 }
