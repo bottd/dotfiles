3d1f54ac8a705c7efc8f9bb00bf43cbdcb727bd7
installer <installer@gmail.com>
2025-05-02 10:52:54 -0500

    Update, game settings

--- nixOS/pocket/configuration.nix
+++ nixOS/pocket/configuration.nix
@@ -79,15 +79,8 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
-  boot.initrd.kernelModules = ["amdgpu"];
-
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
 }
