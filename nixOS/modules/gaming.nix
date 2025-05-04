04e36c8babad393a9ebcafe36856c45b9bbdc498
installer <installer@gmail.com>
2025-05-04 10:17:05 -0500

    Proton GE

--- nixOS/modules/gaming.nix
+++ nixOS/modules/gaming.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  system,
+  ...
+}: {
   programs = {
     gamemode = {
       enable = true;
@@ -11,6 +16,9 @@
       enable = true;
       remotePlay.openFirewall = true;
       gamescopeSession.enable = true;
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
     };
   };
   environment.systemPackages = with pkgs; [
