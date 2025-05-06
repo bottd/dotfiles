1980ed0f7a1d5d1d1f0064136b57e4cc7b825af6
installer <installer@gmail.com>
2025-05-05 20:47:25 -0500

    Update

--- home/linux/hyprland/theme.nix
+++ home/linux/hyprland/theme.nix
@@ -8,13 +8,14 @@
     ".config/hypr/mocha.conf" = {
       source = pkgs.fetchurl {
         url = "https://raw.githubusercontent.com/catppuccin/hyprland/refs/tags/v1.3/themes/mocha.conf";
-        sha256 = "sha256-xYhmqYTHF+nlJVIlNDY4Fyd6moEv6Z8YISTKmpX/p6k=";
+        sha256 = "sha256-SxVNvZZjfuPA2yB9xA0EHHEnE9eIQJAFVBIUuDiSIxQ=";
       };
     };
     ".config/hypr/latte.conf" = {
       source = pkgs.fetchurl {
         url = "https://raw.githubusercontent.com/catppuccin/hyprland/refs/tags/v1.3/themes/latte.conf";
-        sha256 = "sha256-xYhmqYTHF+nlJVIlNDY4Fyd6moEv6Z8YISTKmpX/p6k=";
+        # sha256 = "sha256-xYhmqYTHF+nlJVIlNDY4Fyd6moEv6Z8YISTKmpX/p6k=";
+        sha256 = "sha256-kdflq+y1F8jQI1oFtl6Of31VTW7YdLvSaulPuve7Rz0=";
       };
     };
   };
