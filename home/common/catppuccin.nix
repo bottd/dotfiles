a4ea11df69a36ab906be89fae92747a5f3aef4b5
Drake Bott <bottd@users.noreply.github.com>
2025-05-28 22:24:52 -0500

    Tmp (#19)

---
+++ home/common/catppuccin.nix
@@ -0,0 +1,27 @@
+{ ... }:
+{
+  catppuccin = {
+    flavor = "mocha";
+
+    bat.enable = true;
+    btop.enable = true;
+    fish.enable = true;
+    fzf.enable = true;
+    starship.enable = true;
+    zsh-syntax-highlighting.enable = true;
+
+    delta.enable = true;
+    lazygit.enable = true;
+
+    micro.enable = true;
+    yazi.enable = true;
+    zellij.enable = true;
+
+    cursors.enable = true;
+    cursors.flavor = "mocha";
+    gtk.enable = true;
+    hyprland.enable = false;
+
+    thunderbird.enable = true;
+  };
+}
