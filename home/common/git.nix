0bbc4024dd161f72f284af1767397f34349cd918
Drake Bott <me@drake.dev>
2025-05-29 14:36:11 -0500

    No more github desktop

--- home/common/git.nix
+++ home/common/git.nix
@@ -10,17 +10,7 @@
 
   home.packages = with pkgs; [
     gh
-    github-desktop
     git-filter-repo
     lazygit
   ];
-
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "x-scheme-handler/x-github-desktop-dev-auth" = [ "github-desktop.desktop" ];
-      "x-scheme-handler/x-github-desktop-auth" = [ "github-desktop.desktop" ];
-      "x-scheme-handler/x-github-client-auth" = [ "github-desktop.desktop" ];
-    };
-  };
 }
