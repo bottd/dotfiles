a8357e16c3b68709a5bb7b473496e7067c286bd8
Drake Bott <me@drake.dev>
2025-06-03 19:38:16 -0500

    Remove Java development tools and enhance Zelij keybindings

--- home/common/language/java.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    jdt-language-server
-    vscode-extensions.vscjava.vscode-java-test
-    vscode-extensions.vscjava.vscode-java-debug
-    vscode-extensions.vscjava.vscode-gradle
-  ];
-}
