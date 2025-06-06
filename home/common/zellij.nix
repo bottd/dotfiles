181b4403615f4ccd13179d154831f21ecaca68b7
Drake Bott <me@drake.dev>
2025-06-06 11:57:44 -0500

    Update

---
+++ home/common/zellij.nix
@@ -0,0 +1,34 @@
+{ ... }:
+{
+  programs.zellij = {
+    enable = true;
+    settings = {
+      theme = "catppuccin-mocha";
+
+      # Start in locked mode to prevent keybind conflicts with Neovim
+      default_mode = "locked";
+
+      keybinds = {
+        "unbind \"Ctrl g\"" = true;
+        "locked" = {
+          "bind \"Ctrl a\"" = {
+            SwitchToMode = "tmux";
+          };
+        };
+        "tmux" = {
+          "bind \"Ctrl a\"" = {
+            SwitchToMode = "locked";
+          };
+          "bind \"Esc\"" = {
+            SwitchToMode = "locked";
+          };
+        };
+      };
+    };
+  };
+
+  home.shellAliases = {
+    # TODO: fix alias, theme arg does not exist
+    # zellij = ''zellij --theme "catppuccin-$([[ "$WINDOW_APPEARANCE" == "light" ]] && echo "latte" || echo "mocha")"'';
+  };
+}
