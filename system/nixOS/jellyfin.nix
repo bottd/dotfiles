ee6a14932ee09cfd0a2f4b9fe23e29a0955a6012
Drake Bott <bottd@users.noreply.github.com>
2025-06-12 18:46:56 -0500

    Update

---
+++ system/nixOS/jellyfin.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  # Add jellyfin to the audio group for hardware acceleration (optional)
+  users.users.jellyfin = {
+    extraGroups = [ "audio" "video" "render" ];
+  };
+}
