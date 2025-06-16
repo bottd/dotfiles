b1fe09c957aeac13f64d2393435d9a2eeafb2c62
Drake Bott <bottd@users.noreply.github.com>
2025-06-16 10:50:10 -0500

    floaterm

--- home/common/neovim/lua/plugins/floaterm.lua
+++ home/common/neovim/lua/plugins/floaterm.lua
@@ -1,4 +1,16 @@
+require("floaterm").setup({
+	mappings = {
+		term = function(buf)
+			-- in terminal <C-q> to quick toggle
+			vim.keymap.set({ "n", "t" }, "<C-q>", function()
+				require("floaterm").toggle()
+			end, { buffer = buf })
+		end,
+	},
+})
+
 local wk = require("which-key")
+
 wk.add({
-	{ "<leader>t", ":FloatermToggle", desc = "Floaterm" },
+	{ "<leader>t", ":FloatermToggle<Cr>", desc = "Floaterm" },
 })
