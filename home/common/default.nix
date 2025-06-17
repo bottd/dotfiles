6e7eeb9995ebb34433518b30296f1fd5915d45cc
Drake Bott <bottd@users.noreply.github.com>
2025-06-17 15:31:13 -0500

    B (#21)
    * update
    
    * Update
    
    * Fetch starship palette from github
    
    * Update
    
    * Update

--- home/common/default.nix
+++ home/common/default.nix
@@ -1,6 +1,6 @@
 {
   imports = [
-    ./language
+    ./language.nix
     ./neovim
     ./nushell
     ./starship
---
+++ home/common/language.nix
@@ -0,0 +1,47 @@
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
+    # Rust
+    cargo-binstall
+    (rustup.overrideAttrs (_oldAttrs: { doCheck = false; }))
+
+    # JavaScript/Node
+    nodejs
+    nodePackages_latest.svelte-language-server
+    nodePackages_latest.typescript-language-server
+    nodePackages_latest.prettier
+    prettierd
+    stylelint-lsp
+    tailwindcss-language-server
+    vscode-langservers-extracted
+
+    # Lua
+    lua5_1
+    lua51Packages.luarocks
+    lua-language-server
+    lux-cli
+    stylua
+
+    # Clojure
+    clojure
+    leiningen
+    cljfmt
+    clj-kondo
+    babashka
+    jet
+
+    # Janet
+    janet
+    jpm
+
+    # Common Lisp
+    sbcl
+
+    # TOML
+    taplo
+  ];
+
+  # NPM configuration
+  home.file.".npmrc".text = ''
+    prefix = ~/.npm-packages
+  '';
+}
