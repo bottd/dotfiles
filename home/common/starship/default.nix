668733fa6b4df3319fa21d47c1b992bf93a2e775
Drake Bott <me@drake.dev>
2025-06-17 15:24:39 -0500

    Update

--- home/common/starship/default.nix
+++ home/common/starship/default.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
 let
-  # Fetch catppuccin starship themes from GitHub
   catppuccinStarship = pkgs.fetchFromGitHub {
     owner = "catppuccin";
     repo = "starship";
@@ -8,40 +7,42 @@ let
     sha256 = "sha256-1w0TJdQP5lb9jCrCmhPlSexf0PkAlcz8GBDEsRjPRns=";
   };
 
-  makeStarshipTheme = palette: ''
-    palette = "${if palette == "light" then "catppuccin_latte" else "catppuccin_mocha"}"
-    format = """
-    [┌$fill](bold blue)
-    [│](bold blue) $time
-    [│](bold blue) $all
-    [└───> ](bold blue)"""
+  makeStarshipTheme = palette: {
+    text = ''
+      palette = "${if palette == "light" then "catppuccin_latte" else "catppuccin_mocha"}"
+      format = """
+      [┌$fill](bold blue)
+      [│](bold blue) $time
+      [│](bold blue) $all
+      [└───> ](bold blue)"""
 
-    [character]
-    disabled = true
+      [character]
+      disabled = true
 
-    [directory]
-    truncation_length = 4
-    style = "bold lavender"
+      [directory]
+      truncation_length = 4
+      style = "bold lavender"
 
-    [fill]
-    symbol = "─"
-    style = "bold lavender"
+      [fill]
+      symbol = "─"
+      style = "bold lavender"
 
-    [git_branch]
-    style = "bold maroon"
+      [git_branch]
+      style = "bold maroon"
 
-    [line_break]
-    disabled = true
+      [line_break]
+      disabled = true
 
-    [time]
-    disabled = false
-    style = "italic sky"
-    format = "[$time]($style)"
-    time_format = "%A, %B %e at %I:%M%P"
+      [time]
+      disabled = false
+      style = "italic sky"
+      format = "[$time]($style)"
+      time_format = "%A, %B %e at %I:%M%P"
 
-    ${builtins.readFile "${catppuccinStarship}/themes/latte.toml"}
-    ${builtins.readFile "${catppuccinStarship}/themes/mocha.toml"}
-  '';
+      ${builtins.readFile "${catppuccinStarship}/themes/latte.toml"}
+      ${builtins.readFile "${catppuccinStarship}/themes/mocha.toml"}
+    '';
+  };
 in
 {
   programs.starship = {
@@ -52,7 +53,7 @@ in
   };
 
   xdg.configFile = {
-    "starship/starship.dark.toml".text = makeStarshipTheme "dark";
-    "starship/starship.light.toml".text = makeStarshipTheme "light";
+    "starship/starship.dark.toml" = makeStarshipTheme "dark";
+    "starship/starship.light.toml" = makeStarshipTheme "light";
   };
 }
