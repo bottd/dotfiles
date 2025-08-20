62f038f17a1aa3318fa565fe93c51431843d2866
Drake Bott <bottd@users.noreply.github.com>
2025-08-20 14:56:05 -0500

    Update

--- home/common/neovim/default.nix
+++ home/common/neovim/default.nix
@@ -17,47 +17,48 @@
       text =
         #lua
         ''
-                      local rocks_config = {
-                        rocks_path = vim.fn.stdpath("data") .. "/rocks",
-                        luarocks_binary = "${pkgs.lua51Packages.luarocks}/bin/luarocks",
-                        luarocks_config = {
-                          arch = "${
-            if pkgs.stdenv.hostPlatform.isDarwin
-            then "macosx-aarch64"
-            else "linux-x86_64"
-          }"
-                        }
-                      }
+            local rocks_config = {
+              rocks_path = vim.fn.stdpath("data") .. "/rocks",
+              luarocks_binary = "${pkgs.lua51Packages.luarocks}/bin/luarocks",
+              luarocks_config = {
+                arch = "${
+                  if pkgs.stdenv.hostPlatform.isDarwin
+                    then "macosx-aarch64"
+                  else "linux-x86_64"
+                }"
+              }
+            }
 
-                    vim.g.rocks_nvim = rocks_config
+          vim.g.rocks_nvim = rocks_config
 
-                      local luarocks_path = {
-                        vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
-                        vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
-                      }
-                    package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
+            local luarocks_path = {
+              vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
+              vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
+            }
+          package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
 
-                      local luarocks_cpath = {
-                        vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
-                        vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
-                      }
-                    package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
+            local luarocks_cpath = {
+              vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
+              vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
+            }
+          package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
 
-                      if not vim.uv.fs_stat(rocks_config.rocks_path) then
-                        vim.system({
-                            rocks_config.luarocks_binary,
-                            "install",
-                            "--tree",
-                            rocks_config.rocks_path,
-                            "--server=\'https://nvim-neorocks.github.io/rocks-binaries/\'",
-                            "--lua-version=5.1",
-                            "rocks.nvim",
-                            }):wait()
-                               end
+            if not vim.uv.fs_stat(rocks_config.rocks_path) then
+              vim.system({
+                  rocks_config.luarocks_binary,
+                  "install",
+                  "--tree",
+                  rocks_config.rocks_path,
+                  "--server=\'https://nvim-neorocks.github.io/rocks-binaries/\'",
+                  "--lua-version=5.1",
+                  "rocks.nvim",
+                  }):wait()
+                     end
 
-                                 vim.opt.runtimepath:append(vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*"))
+                       vim.opt.runtimepath:append(vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*"))
 
-          require("general-config")
+                       require("hotpot")
+                       require("general-config")
         '';
     };
 
