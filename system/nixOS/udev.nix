b27d2605522f20d9ba9fc63cbac0c8f16d1d9753
Drake Bott <bottd@users.noreply.github.com>
2025-08-29 10:35:10 -0500

    Update

---
+++ system/nixOS/udev.nix
@@ -0,0 +1,8 @@
+{ ... }:
+{
+  # Rule for VIA keyboard detection
+  services.udev.extraRules = ''
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+  '';
+}
+
