d01d11facea05be8d031ed9d93f0f193d6b147a4
Drake Bott <bott.da@gmail.com>
2025-09-13 15:16:06 -0500

    Update

--- home/common/syncthing.nix
+++ home/common/syncthing.nix
@@ -1,7 +1,24 @@
-{ pkgs, ... }:
+{ pkgs, neorgWorkspace, ... }:
 {
   services.syncthing = {
     enable = true;
     tray.enable = pkgs.stdenv.hostPlatform.isLinux;
+    settings = {
+      folders = {
+        "${neorgWorkspace}" = {
+          path = "~/${neorgWorkspace}";
+          devices = [ "cellar-pi" ];
+          versioning = {
+            type = "simple";
+            params.keep = "10";
+          };
+        };
+      };
+      options = {
+        urAccepted = -1;
+        relaysEnabled = true;
+        localAnnounceEnabled = true;
+      };
+    };
   };
 }
