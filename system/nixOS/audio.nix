979ef918afb1f309ef134f91c20ce6bc34d9ef60
Drake Bott <bottd@users.noreply.github.com>
2025-10-03 18:46:39 +0000

    audio

--- system/nixOS/audio.nix
+++ system/nixOS/audio.nix
@@ -7,5 +7,20 @@
     pulse.enable = true;
     jack.enable = true;
     wireplumber.enable = true;
+
+    extraConfig.pipewire."92-game-audio-sink" = {
+      "context.objects" = [
+        {
+          factory = "adapter";
+          args = {
+            "factory.name" = "support.null-audio-sink";
+            "node.name" = "GameAudio";
+            "node.description" = "Game Audio (Recording)";
+            "media.class" = "Audio/Sink";
+            "audio.position" = [ "FL" "FR" ];
+          };
+        }
+      ];
+    };
   };
 }
