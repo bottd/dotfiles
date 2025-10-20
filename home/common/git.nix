77294f7b5e7326524b7763ef7d8829aa1444f974
Drake Bott <bott.da@gmail.com>
2025-10-19 22:35:21 -0500

    Update

--- home/common/git.nix
+++ home/common/git.nix
@@ -1,23 +1,25 @@
 { pkgs, ... }: {
   programs.git = {
     enable = true;
-    userEmail = "bottd@users.noreply.github.com";
-    delta = {
-      enable = true;
-      options = {
-        navigate = true;
-        light = false;
-        side-by-side = true;
-        line-numbers = true;
-      };
-    };
-    extraConfig = {
+    settings = {
+      user.email = "bottd@users.noreply.github.com";
       push = {
         autoSetupRemote = true;
       };
       init.defaultBranch = "main";
       merge.conflictstyle = "diff3";
       diff.colorMoved = "default";
+    };
+  };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      navigate = true;
+      light = false;
+      side-by-side = true;
+      line-numbers = true;
     };
   };
 
