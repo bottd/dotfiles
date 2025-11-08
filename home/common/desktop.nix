c0167f31010c10b09e9203c394da7f402df51128
Drake Bott <bottd@users.noreply.github.com>
2025-11-08 11:27:00 -0600

    Move chromium to linux desktop module

--- home/common/desktop.nix
+++ home/common/desktop.nix
@@ -2,7 +2,6 @@
 , ...
 }: {
   home.packages = with pkgs; [
-    chromium
     firefox
     inkscape
   ];
--- home/linux/desktop.nix
+++ home/linux/desktop.nix
@@ -13,6 +13,7 @@ let
 in
 {
   home.packages = with pkgs; [
+    chromium
     vesktop
     flashprint
     filezilla
