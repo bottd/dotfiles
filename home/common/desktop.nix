a82ace778faf3306da8db745b2d05fb810e7076a
Drake Bott <me@drake.dev>
2025-11-22 17:01:42 -0600

    firefox accessibility

--- home/common/desktop.nix
+++ home/common/desktop.nix
@@ -7,6 +7,24 @@
       settings = {
         "accessibility.force_disabled" = 1;
       };
+      search = {
+        default = "Kagi";
+        force = true;
+        engines = {
+          "Kagi" = {
+            urls = [
+              {
+                template = "https://kagi.com/search?q={searchTerms}";
+              }
+              {
+                template = "https://kagi.com/api/autosuggest?q={searchTerms}";
+                type = "application/x-suggestions+json";
+              }
+            ];
+            definedAliases = [ "@k" ];
+          };
+        };
+      };
     };
   };
 
