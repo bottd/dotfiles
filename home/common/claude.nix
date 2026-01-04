834c6731f4761e5ef967a46b815167e076b02ff0
Drake Bott <me@drake.dev>
2026-01-03 20:16:36 -0600

    glm setup

--- home/common/claude.nix
+++ home/common/claude.nix
@@ -1,48 +1,60 @@
 { desktopEnvironment ? null, lib, pkgs, inputs, ... }: {
-  home.packages = lib.optionals (desktopEnvironment != null && pkgs.stdenv.isLinux) [
-    inputs.claude-desktop.packages.${pkgs.system}.claude-desktop-with-fhs
-  ];
+  home = {
+    packages = lib.optionals (desktopEnvironment != null && pkgs.stdenv.isLinux) [
+      inputs.claude-desktop.packages.${pkgs.system}.claude-desktop-with-fhs
+    ];
+
+    sessionVariables = {
+      ANTHROPIC_BASE_URL = "https://api.z.ai/api/anthropic";
+      API_TIMEOUT_MS = "3000000";
+    };
+
+    file =
+      let
+        claudeConfig =
+          # json
+          ''
+            {
+              "model": "opus",
+              "env": {
+                "ANTHROPIC_AUTH_TOKEN": "$ANTHROPIC_AUTH_TOKEN",
+                "ANTHROPIC_BASE_URL": "https://api.z.ai/api/anthropic",
+                "API_TIMEOUT_MS": "3000000"
+              },
+              "permissions": {
+                "allow": [
+                  "mcp__svelte__*"
+                ]
+              },
+              "plugins": {},
+              "mcpServers": {
+                "svelte": {
+                  "command": "npx",
+                  "args": [
+                    "-y",
+                    "@sveltejs/mcp"
+                  ]
+                }
+              }
+            }
+          '';
+      in
+      {
+        # claude-code config
+        ".claude/settings.json" = {
+          text = claudeConfig;
+        };
+
+        # claude desktop config
+        ".config/Claude/claude_desktop_config.json" = {
+          text = claudeConfig;
+        };
+      };
+  };
 
   programs.git.ignores = [
     ".claude/settings.local.json"
   ];
-
-  home.file =
-    let
-      claudeConfig =
-        # json
-        ''
-          {
-            "model": "opus",
-            "permissions": {
-              "allow": [
-                "mcp__svelte__*"
-              ]
-            },
-            "plugins": {},
-            "mcpServers": {
-              "svelte": {
-                "command": "npx",
-                "args": [
-                  "-y",
-                  "@sveltejs/mcp"
-                ]
-              }
-            }
-          }
-        '';
-    in
-    {
-      # claude-code config
-      ".claude/settings.json" = {
-        text = claudeConfig;
-      };
-
-      # claude desktop config
-      ".config/Claude/claude_desktop_config.json" = {
-        text = claudeConfig;
-      };
-    };
 
   xdg.mimeApps = lib.mkIf (desktopEnvironment != null && pkgs.stdenv.isLinux) {
     associations.added = {
