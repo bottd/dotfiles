a609ecbb60bbcddf94d4281ca5b05a68cea4393d
Drake Bott <me@drake.dev>
2026-01-14 23:07:29 -0600

    Update

--- home/linux/games/counter-strike.nix
+++ home/linux/games/counter-strike.nix
@@ -3,10 +3,10 @@ _:
   home.file.".local/share/Steam/steamapps/common/Counter-Strike Global Offensive/game/csgo/cfg/autoexec.cfg".text = ''
     cl_radar_square_always "1"
     sensitivity "0.75"
-    bind "g" "slot7"
-    bind "f" "slot6"
-    bind "x" "slot8"
-    bind "z" "slot9"
+    bind "g" "slot7" // HE Grenade
+    bind "f" "slot6" // Flashbang
+    bind "x" "slot8" // Molotov
+    bind "z" "slot9" // Decoy
     alias "exit" "quit"
   '';
 }
