8b5d865f656718871bd9b8e08b9ee5a65640ecf7
Drake Bott <me@drake.dev>
2026-01-16 15:00:10 -0600

    Fix he/flash keys

--- home/linux/games/counter-strike.nix
+++ home/linux/games/counter-strike.nix
@@ -3,10 +3,9 @@ _:
   home.file.".local/share/Steam/steamapps/common/Counter-Strike Global Offensive/game/csgo/cfg/autoexec.cfg".text = ''
     cl_radar_square_always "1"
     sensitivity "0.75"
-    bind "g" "slot7" // HE Grenade
-    bind "f" "slot6" // Flashbang
+    bind "g" "slot6" // HE Grenade
+    bind "f" "slot7" // Flashbang
     bind "x" "slot8" // Molotov
     bind "z" "slot9" // Decoy
-    alias "exit" "quit"
   '';
 }
