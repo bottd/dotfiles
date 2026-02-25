0852f431e4d29a6382e7a0ef5123254d01dc7c58
Drake Bott <me@drake.dev>
2026-02-25 15:50:04 -0600

    update

--- hosts/macbook/configuration.nix
+++ hosts/macbook/configuration.nix
@@ -1,6 +1,10 @@
 { inputs
+, username
 , ...
 }: {
   # Set Git commit hash for darwin-version.
   system.configurationRevision = inputs.rev or inputs.dirtyRev or null;
+
+  # Required by nix-darwin 25.11 for user-scoped options
+  system.primaryUser = username;
 }
