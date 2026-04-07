0cf7489d3cea027f6cf251b2925147ddf052babb
Drake Bott <me@drake.dev>
2026-04-07 09:07:47 -0500

    add default hashed pw

--- system/users/default.nix
+++ system/users/default.nix
@@ -8,6 +8,7 @@
     (lib.mkIf pkgs.stdenv.isLinux {
       isNormalUser = true;
       extraGroups = [ "networkmanager" "wheel" ];
+      hashedPassword = "$6$RWnDqI6YSUzRNFcH$mYbS.1KQUPaNYRqK9C2So4oPy2hG7/sKCDDzDffv0jYkGk7g5O7uj8qWvMIRJi9kpmPOS5T3q49djmsYIhtyY.";
     })
     (lib.mkIf pkgs.stdenv.isDarwin { })
   ];
