e918058cc193cb156a29642d520ce7bdf8909a78
Drake Bott <me@drake.dev>
2026-04-10 08:50:52 -0500

    update

--- home/common/ghostty.nix
+++ home/common/ghostty.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, theme, ... }: {
+{ lib, pkgs, ... }: {
   # Ghostty `command` option fails to launch zellij on darwin
   # start zellij via zsh instead on darwin
   programs.zsh.initContent = lib.optionalString pkgs.stdenv.isDarwin ''
@@ -7,39 +7,34 @@
     fi
   '';
 
-  home.file = {
-    ".config/ghostty/config" = {
-      text = ''
-        ${lib.optionalString pkgs.stdenv.isLinux "command = zellij"}
-        desktop-notifications = false
+  programs.ghostty = {
+    enable = true;
+    package = if pkgs.stdenv.isLinux then pkgs.ghostty else null;
+    settings = {
+      command = lib.mkIf pkgs.stdenv.isLinux "zellij";
+      desktop-notifications = false;
 
-        font-family = MonoLisa Variable
-        font-family-bold = MonoLisa Variable Regular Bold
-        font-family-italic = MonoLisa Variable Italic Italic
-        font-family-bold-italic = MonoLisa Variable Italic Bold Italic
-        font-feature = ss02
-        font-size = ${toString theme.baseFontSize}
+      font-family = lib.mkForce "MonoLisa Variable";
+      font-family-bold = "MonoLisa Variable Regular Bold";
+      font-family-italic = "MonoLisa Variable Italic Italic";
+      font-family-bold-italic = "MonoLisa Variable Italic Bold Italic";
+      font-feature = "ss02";
 
-        macos-non-native-fullscreen = true
+      macos-non-native-fullscreen = true;
 
-        mouse-hide-while-typing = true
+      mouse-hide-while-typing = true;
 
-        quit-after-last-window-closed = true
+      quit-after-last-window-closed = true;
 
-        shell-integration = none
+      shell-integration = "none";
 
-        background-opacity = 1.0
+      background-opacity = 1.0;
 
-        gtk-titlebar = true
+      gtk-titlebar = true;
 
-        macos-titlebar-style = native
+      macos-titlebar-style = "native";
 
-        keybind = alt+enter=unbind
-      '';
+      keybind = "alt+enter=unbind";
     };
   };
-
-  home.packages = lib.optionals pkgs.stdenv.isLinux [
-    pkgs.ghostty
-  ];
 }
