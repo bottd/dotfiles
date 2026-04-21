f7b657e4ed6ecdf1013726bed4594dbffb8f0a08
Drake Bott <me@drake.dev>
2026-04-21 14:45:43 -0500

    firefox default

--- home/common/browser.nix
+++ home/common/browser.nix
@@ -1,6 +1,24 @@
-_: {
+{ lib, pkgs, ... }: {
   home.sessionVariables = {
     BROWSER = "firefox";
+  };
+
+  xdg.mimeApps = lib.mkIf pkgs.stdenv.isLinux {
+    defaultApplications = {
+      "text/html" = "firefox.desktop";
+      "application/xhtml+xml" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
+      "x-scheme-handler/ftp" = "firefox.desktop";
+      "x-scheme-handler/chrome" = "firefox.desktop";
+      "application/x-extension-htm" = "firefox.desktop";
+      "application/x-extension-html" = "firefox.desktop";
+      "application/x-extension-shtml" = "firefox.desktop";
+      "application/x-extension-xhtml" = "firefox.desktop";
+      "application/x-extension-xht" = "firefox.desktop";
+    };
   };
 
   programs.firefox = {
