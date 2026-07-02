80cf6e60cc7dd3aee08aa9e7e3e341d19fd6ef7f
Drake Bott <me@drake.dev>
2026-07-02 12:27:39 -0500

    glide addons

--- home/common/glide/config/glide.ts
+++ home/common/glide/config/glide.ts
@@ -1,3 +1,28 @@
 /// <reference types="./glide.d.ts" />
 
 glide.keymaps.set("normal", "<leader>c", "config_reload");
+
+type Addon = glide.AddonInstallOptions & {
+  url: string;
+};
+const addons: Addon[] = [
+  {
+    url: "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi",
+    private_browsing_allowed: true,
+  },
+  {
+    url: "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi",
+    private_browsing_allowed: true,
+  },
+  {
+    url: "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi",
+  },
+  {
+    url: "https://addons.mozilla.org/firefox/downloads/latest/kagi-search-for-firefox/latest.xpi",
+  },
+  {
+    url: "https://addons.mozilla.org/firefox/downloads/latest/remove-youtube-s-suggestions/latest.xpi",
+  },
+];
+
+addons.forEach(({ url, ...options }) => glide.addons.install(url, options));
