3ebfeff3077fb8cac4a9fae195e77cfa10a7184b
Drake Bott <me@drake.dev>
2026-07-15 22:10:29 -0500

    update

--- home/common/ai/kli/default.nix
+++ home/common/ai/kli/default.nix
@@ -1,8 +1,26 @@
-{ config, inputs, ... }:
+{ config, inputs, pkgs, ... }:
+let
+  configuredKli = inputs.kli.lib.${pkgs.stdenv.hostPlatform.system}.mkConfiguredKli {
+    inherit (config.programs.kli) extensions settings registries blessedNativeLibs dataDir sandbox;
+  };
+in
 {
   imports = [ inputs.kli.homeManagerModules.default ];
 
-  programs.kli.enable = true;
+  programs.kli = {
+    enable = true;
+    # buildLisp's runtime wrapper only sets LD_LIBRARY_PATH; macOS needs DYLD_LIBRARY_PATH.
+    package = pkgs.symlinkJoin {
+      name = "kli";
+      paths = [ configuredKli ];
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        rm "$out/bin/kli"
+        makeWrapper ${configuredKli}/bin/kli "$out/bin/kli" \
+          --prefix DYLD_LIBRARY_PATH : "${pkgs.openssl.out}/lib"
+      '';
+    };
+  };
 
   home.file = {
     ".config/kli/settings.json".source =
