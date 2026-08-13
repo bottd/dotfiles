4495f3ec46e2237d01ed77cd8f2e59fad7844735
Drake Bott <me@drake.dev>
2026-08-13 11:11:22 -0500

    update

--- home/common/emacs/default.nix
+++ home/common/emacs/default.nix
@@ -7,8 +7,21 @@
       epkgs.nix-mode
       epkgs.nixfmt
 
+      epkgs.subed
       epkgs.vterm
     ];
+
+    extraConfig = ''
+      (with-eval-after-load 'subed
+        (add-to-list 'auto-mode-alist '("\\.srt\\'" . subed-srt-mode)))
+      (add-to-list 'auto-mode-alist '("\\.srt\\'" . subed-srt-mode))
+
+      ;; subed bindings:
+      ;;   M-n / M-p  next/previous subtitle      M-SPC    play/pause
+      ;;   M-j        jump player to point        C-c C-l  loop current
+      ;;   C-c ,      toggle player-follows-point
+      ;;   C-c .      toggle point-follows-player
+    '';
   };
 
   services.emacs.enable = true;
