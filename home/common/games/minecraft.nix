16de61ff4f227c7b7cb2ffb8a156a4bc90cc75f7
Drake Bott <me@drake.dev>
2026-08-30 15:51:37 -0500

    update

--- home/common/games/minecraft.nix
+++ home/common/games/minecraft.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   home.packages = with pkgs; [
     (prismlauncher.override {
@@ -6,8 +6,8 @@
         temurin-bin-25
         temurin-bin-21
         temurin-bin-17
-        temurin-bin-8
-      ];
+      ]
+      ++ lib.optional stdenv.isLinux temurin-bin-8;
     })
   ];
 
