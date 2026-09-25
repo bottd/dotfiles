2daaf6962dc007d4e877e48ea772b0fdd5e714e2
Drake Bott <me@drake.dev>
2026-09-25 15:14:47 -0500

    update

--- system/nixOS/tailscale.nix
+++ system/nixOS/tailscale.nix
@@ -1,5 +1,7 @@
-{ username, ... }:
+{ username, features, lib, pkgs, ... }:
 
 {
   services.tailscale.extraSetFlags = [ "--operator=${username}" ];
+
+  environment.systemPackages = lib.optionals features.gui [ pkgs.trayscale ];
 }
