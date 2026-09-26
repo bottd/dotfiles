d0b719abc0bccddf038479b49e1439bafdb25343
Drake Bott <me@drake.dev>
2026-09-26 11:29:00 -0500

    update

--- home/linux/mpv/default.nix
+++ home/linux/mpv/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 let
   mpv-cut-src = pkgs.fetchFromGitHub {
     owner = "familyfriendlymikey";
@@ -16,7 +16,21 @@ let
   '';
 in
 {
-  programs.mpv.enable = true;
+  programs.mpv = {
+    enable = true;
+    scriptOpts.osc = with config.lib.stylix.colors.withHashtag; {
+      background_color = base00;
+      timecode_color = base0D;
+      title_color = base05;
+      time_pos_color = base05;
+      buttons_color = base05;
+      small_buttonsL_color = base05;
+      small_buttonsR_color = base05;
+      top_buttons_color = base05;
+      held_element_color = base03;
+      time_pos_outline_color = base00;
+    };
+  };
 
   xdg.configFile = {
     "mpv/scripts/mpv-cut/main.lua".source = "${mpv-cut-src}/main.lua";
